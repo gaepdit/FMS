85d4a0e874edce2b34122d9e724e848b09d3cc30
Doug Waldron <douglas.waldron@dnr.ga.gov>
2020-08-21 17:27:44 -0400

    Add latitude/longitude EF migration

---
+++ FMS.Infrastructure/Migrations/20200821212503_LatLongDataTypes.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FMS.Infrastructure.Migrations
+{
+    public partial class LatLongDataTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Longitude",
+                table: "Facilities",
+                type: "decimal(9, 6)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Latitude",
+                table: "Facilities",
+                type: "decimal(8, 6)",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Longitude",
+                table: "Facilities",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(9, 6)");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Latitude",
+                table: "Facilities",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(8, 6)");
+        }
+    }
+}
