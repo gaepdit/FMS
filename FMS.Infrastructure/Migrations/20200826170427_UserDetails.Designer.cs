b30d669ba1eb94daa0774c3736c922ac48f538d3
Doug Waldron <douglas.waldron@dnr.ga.gov>
2020-08-26 13:06:26 -0400

    Add EF migration for ApplicationUser (Identity)
    see #5
    
    dotnet ef migrations add UserDetails -s FMS -p FMS.Infrastructure

--- FMS.Infrastructure/Migrations/20200826170427_UserDetails.Designer.cs
+++ FMS.Infrastructure/Migrations/20200826170427_UserDetails.Designer.cs
@@ -4,14 +4,16 @@ using FMS.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FMS.Infrastructure.Migrations
 {
     [DbContext(typeof(FmsDbContext))]
-    partial class FmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200826170427_UserDetails")]
+    partial class UserDetails
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
