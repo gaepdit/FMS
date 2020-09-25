e5237cfe462b716cdd0bb62394efd1bac2996982
Doug Waldron <douglas.waldron@dnr.ga.gov>
2020-09-25 12:23:22 -0400

    Add unique index to Facility Number
    adds EF migration
    
    ref #66

--- FMS.Infrastructure/Contexts/FmsDbContext.cs
+++ FMS.Infrastructure/Contexts/FmsDbContext.cs
@@ -36,9 +36,10 @@ namespace FMS.Infrastructure.Contexts
             // Configure many-to-many relationships
             builder.Entity<CabinetFile>().HasKey(e => new { e.CabinetId, e.FileId });
 
-            // Additional model properties
+            // Unique indexes
             builder.Entity<File>().HasIndex(e => e.FileLabel).IsUnique();
             builder.Entity<Cabinet>().HasIndex(e => e.Name).IsUnique();
+            builder.Entity<Facility>().HasIndex(e => e.FacilityNumber).IsUnique();
 
             // Identity Tables
             builder.Entity<ApplicationUser>().ToTable("AppUsers");
