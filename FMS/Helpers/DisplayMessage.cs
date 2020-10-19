gaepdit/FMS

commit 535676d642d066a35f4e5d7f5532cd9040b9757e

Refactor CSV helper and add unit tests

FMS/Helpers/DisplayMessage.cs
@@ -2,10 +2,9 @@
 {
     public class DisplayMessage
     {
-        public Context Context { get; set; }
-        public string Message { get; set; }
+        private Context Context { get; }
+        public string Message { get; }
 
-        public DisplayMessage() { }
         public DisplayMessage(Context context, string message)
         {
             Context = context;

FMS.Infrastructure/SeedData/FacilitySeedData.cs
@@ -208,7 +208,29 @@ namespace FMS.Infrastructure.SeedData
                     Latitude = 34.141353m,
                     Longitude = -84.505629m,
                     CountyId = 243, // CHEROKEE
-                }
+                },
+                new Facility
+                {
+                    Id = new Guid("810DDE72-5459-4ECC-81D8-A51554C9FF3F"),
+                    Active = true,
+                    FacilityNumber = "UTF8",
+                    FileId = new Guid("EF5FB128-D3BF-4CFF-9931-9F114D25D8A1"), // 180-0001
+                    EnvironmentalInterestId = new Guid("FC2A0444-6287-432F-9285-6BA0E7AA73C6"), //RCRA
+                    FacilityTypeId = new Guid("3FE94D7D-563E-4CA1-A094-BB6E217990D2"), //gen
+                    OrganizationalUnitId = new Guid("57B8BEB5-368A-4056-872D-0DB0ADE175E3"), //Org Unit
+                    BudgetCodeId = new Guid("457D191A-D2B1-4C38-8633-9061C4268E37"), //HWRCRA
+                    Name = "Unicode’s fun 🐛👍😜",
+                    ComplianceOfficerId = new Guid("468F746A-270F-4584-8B04-71CD5271A40F"), //00943668
+                    FacilityStatusId = new Guid("0FF0A063-2D11-4305-BADA-E9A4414EDDF1"),
+                    Location = "Parking lot",
+                    Address = "123 Street",
+                    City = "Town",
+                    State = "Georgia",
+                    PostalCode = "30188",
+                    Latitude = 34.1m,
+                    Longitude = -84.5m,
+                    CountyId = 243,
+                },
             };
         }
     }

FMS/Helpers/CsvHelper.cs
@@ -1,0 +1,44 @@
+﻿using System.Collections;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace FMS
+{
+    public static class CsvHelper
+    {
+        public static async Task<byte[]> GetCsvByteArrayAsync<T>(this IEnumerable list)
+            where T : ClassMap
+        {
+            MemoryStream ms = null;
+            StreamWriter writer = null;
+            CsvWriter csv = null;
+
+            try
+            {
+                ms = new MemoryStream();
+                writer = new StreamWriter(ms, Encoding.UTF8);
+                csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+                csv.Configuration.SanitizeForInjection = true;
+                csv.Configuration.RegisterClassMap<T>();
+                await csv.WriteRecordsAsync(list);
+
+                await csv.FlushAsync();
+                await writer.FlushAsync();
+                await ms.FlushAsync();
+
+                return ms.ToArray();
+            }
+            finally
+            {
+                if (csv != null) await csv.DisposeAsync();
+                if (writer != null) await writer.DisposeAsync();
+                if (ms != null) await ms.DisposeAsync();
+            }
+        }
+    }
+}

FMS/Pages/Facilities/Map.cshtml.cs
@@ -1,18 +1,11 @@
-﻿using FMS.Domain.Data;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
 using FMS.Domain.Dto;
 using FMS.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CsvHelper;
-using CsvHelper.Configuration;
-using System;
-using System.IO;
-using System.Globalization;
-
 
 namespace FMS.Pages.Facilities
 {
@@ -79,10 +72,10 @@ namespace FMS.Pages.Facilities
 
             ExportSpec = new FacilityMapSpec()
             {
-                Latitude=spec.Latitude,
-                Longitude=spec.Longitude,
-                ActiveOnly=spec.ActiveOnly,
-                Radius=spec.Radius
+                Latitude = spec.Latitude,
+                Longitude = spec.Longitude,
+                ActiveOnly = spec.ActiveOnly,
+                Radius = spec.Radius
             };
             return Page();
         }
@@ -90,43 +83,11 @@ namespace FMS.Pages.Facilities
         public async Task<IActionResult> OnPostAsync()
         {
             FacilityList = await _repository.GetFacilityListAsync(ExportSpec);
-            return File(await GetCsvByteArrayAsync(), "text/csv",
-                $"FMS_export_{DateTime.Now:yyyy-MM-dd-HH-mm-ss.FFF}.csv");
+            return File(await FacilityList.GetCsvByteArrayAsync<FacilityMapReportMap>(), "text/csv",
+                $"FMS_export_{DateTime.Now:yyyy-MM-dd.HH-mm-ss.FFF}.csv");
         }
 
-        #region "Reports"
-
-        private async Task<byte[]> GetCsvByteArrayAsync()
-        {
-            MemoryStream ms = null;
-            StreamWriter writer = null;
-            CsvWriter csv = null;
-
-            try
-            {
-                ms = new MemoryStream();
-                writer = new StreamWriter(ms, new System.Text.UTF8Encoding(true));
-                csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
-
-                csv.Configuration.SanitizeForInjection = true;
-                csv.Configuration.RegisterClassMap<FacilityMapReportMap>();
-                await csv.WriteRecordsAsync(FacilityList);
-
-                await csv.FlushAsync();
-                await writer.FlushAsync();
-                await ms.FlushAsync();
-
-                return ms.ToArray();
-            }
-            finally
-            {
-                if (csv != null) await csv.DisposeAsync();
-                if (writer != null) await writer.DisposeAsync();
-                if (ms != null) await ms.DisposeAsync();
-            }
-        }
-
-        private class FacilityMapReportMap : ClassMap<FacilityMapSummaryDto>
+        private sealed class FacilityMapReportMap : ClassMap<FacilityMapSummaryDto>
         {
             public FacilityMapReportMap()
             {
@@ -135,16 +96,14 @@ namespace FMS.Pages.Facilities
                 Map(m => m.Name).Index(2).Name("Facility Name");
                 Map(m => m.Address).Index(3).Name("Street Address");
                 Map(m => m.City).Index(4).Name("City");
-                Map(m => m.State).Index(6).Name("State");
-                Map(m => m.PostalCode).Index(7).Name("ZIP Code");
-                Map(m => m.FacilityType).Index(9).Name("Facility Type");
-                Map(m => m.FacilityStatus).Index(14).Name("Facility Status");
-                Map(m => m.Latitude).Index(12).Name("Latitude");
-                Map(m => m.Longitude).Index(13).Name("Longitude");
-                Map(m => m.Distance).Index(15).Name("Distance (miles)");
+                Map(m => m.State).Index(5).Name("State");
+                Map(m => m.PostalCode).Index(6).Name("ZIP Code");
+                Map(m => m.FacilityType).Index(7).Name("Facility Type");
+                Map(m => m.Latitude).Index(8).Name("Latitude");
+                Map(m => m.Longitude).Index(9).Name("Longitude");
+                Map(m => m.FacilityStatus).Index(10).Name("Facility Status");
+                Map(m => m.Distance).Index(11).Name("Distance (miles)");
             }
         }
-
-        #endregion
     }
 }

FMS/Pages/Reports/Index.cshtml.cs
@@ -1,9 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
 using System.Threading.Tasks;
-using CsvHelper;
 using CsvHelper.Configuration;
 using FMS.Domain.Data;
 using FMS.Domain.Dto;
@@ -58,8 +55,8 @@ namespace FMS.Pages.Reports
         public async Task<IActionResult> OnPostAsync()
         {
             FacilityList = await _repository.GetFacilityDetailListAsync(Spec);
-
-            return File(await GetCsvByteArrayAsync(), "text/csv", $"FMS_export_{DateTime.Now:yyyy-MM-dd-HH-mm-ss.FFF}.csv");
+            return File(await FacilityList.GetCsvByteArrayAsync<FacilityReportMap>(), "text/csv",
+                $"FMS_export_{DateTime.Now:yyyy-MM-dd.HH-mm-ss.FFF}.csv");
         }
 
         private async Task SetNamesAsync()
@@ -67,45 +64,14 @@ namespace FMS.Pages.Reports
             CountyName = Data.Counties.Find(e => e.Id == Spec.CountyId)?.Name;
             BudgetCodeName = await _listRepository.GetBudgetCodeNameAsync(Spec.BudgetCodeId);
             ComplianceOfficerName = await _listRepository.GetComplianceOfficerNameAsync(Spec.ComplianceOfficerId);
-            EnvironmentalInterestName = await _listRepository.GetEnvironmentalInterestNameAsync(Spec.EnvironmentalInterestId);
+            EnvironmentalInterestName = await _listRepository
+                .GetEnvironmentalInterestNameAsync(Spec.EnvironmentalInterestId);
             FacilityStatusName = await _listRepository.GetFacilityStatusNameAsync(Spec.FacilityStatusId);
             FacilityTypeName = await _listRepository.GetFacilityTypeNameAsync(Spec.FacilityTypeId);
             OrganizationalUnitName = await _listRepository.GetOrganizationalUnitNameAsync(Spec.OrganizationalUnitId);
         }
 
-        #region "Reports"
-
-        public async Task<byte[]> GetCsvByteArrayAsync()
-        {
-            MemoryStream ms = null;
-            StreamWriter writer = null;
-            CsvWriter csv = null;
-
-            try
-            {
-                ms = new MemoryStream();
-                writer = new StreamWriter(ms, new System.Text.UTF8Encoding(true));
-                csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
-
-                csv.Configuration.SanitizeForInjection = true;
-                csv.Configuration.RegisterClassMap<FacilityReportMap>();
-                await csv.WriteRecordsAsync(FacilityList);
-
-                await csv.FlushAsync();
-                await writer.FlushAsync();
-                await ms.FlushAsync();
-
-                return ms.ToArray();
-            }
-            finally
-            {
-                if (csv != null) await csv.DisposeAsync();
-                if (writer != null) await writer.DisposeAsync();
-                if (ms != null) await ms.DisposeAsync();
-            }
-        }
-
-        private class FacilityReportMap : ClassMap<FacilityDetailDto>
+        private sealed class FacilityReportMap : ClassMap<FacilityDetailDto>
         {
             public FacilityReportMap()
             {
@@ -127,7 +93,5 @@ namespace FMS.Pages.Reports
                 Map(m => m.CabinetsToString).Index(15).Name("Cabinet(s)");
             }
         }
-
-        #endregion
     }
 }

tests/FMS.App.Tests/Helpers/CsvHelperTests.cs
@@ -1,0 +1,65 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using CsvHelper.Configuration;
+using FluentAssertions;
+using Xunit;
+using Xunit.Extensions.AssertExtensions;
+
+namespace FMS.App.Tests.Helpers
+{
+    public class CsvHelperTests
+    {
+        private class MyClass
+        {
+            public MyClass(int i, decimal m, double d, float f, string s)
+            {
+                MyInt = i;
+                MyDecimal = m;
+                MyDouble = d;
+                MyFloat = f;
+                MyString = s;
+            }
+
+            public int MyInt { get; }
+            public decimal MyDecimal { get; }
+            public double MyDouble { get; }
+            public float MyFloat { get; }
+            public string MyString { get; }
+        }
+
+        private static readonly List<MyClass> MyItems = new List<MyClass>()
+        {
+            new MyClass(1, 2, 3, 4, "abc"),
+            new MyClass(1, 2.0m, 3.0d, 4.0f, "🐛👍😜"),
+            new MyClass(0, 0, 0, 0, ""),
+            new MyClass(0, 0, 0, 0, null),
+        };
+
+        private sealed class MyClassMap : ClassMap<MyClass>
+        {
+            public MyClassMap()
+            {
+                AutoMap(CultureInfo.InvariantCulture);
+            }
+        }
+
+        private static readonly string Zwnbsp = ((char) 0xFEFF).ToString();
+
+        private static readonly string CsvOutput = $"{Zwnbsp}MyInt,MyDecimal,MyDouble,MyFloat,MyString\r\n" +
+            "1,2,3,4,abc\r\n" +
+            "1,2.0,3,4,🐛👍😜\r\n" +
+            "0,0,0,0,\r\n" +
+            "0,0,0,0,\r\n";
+
+        [Fact]
+        public async Task GetCsv_Succeeds()
+        {
+            var csvByteArray = await MyItems.GetCsvByteArrayAsync<MyClassMap>();
+            var result = System.Text.Encoding.UTF8.GetString(csvByteArray);
+            result.Should().BeEquivalentTo(CsvOutput);
+        }
+    }
+}

commit efb27696499fb0712ad83a1ff7d781d4ebd59a40

Fix bug in DisplayMessage
introduced in 535676d6

FMS/Helpers/DisplayMessage.cs
@@ -5,6 +5,10 @@
         private Context Context { get; }
         public string Message { get; }
 
+        // parameterless constructor required for deserialization in GetDisplayMessage method
+        // ReSharper disable once UnusedMember.Global
+        public DisplayMessage() { }
+
         public DisplayMessage(Context context, string message)
         {
             Context = context;