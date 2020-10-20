2cf89031f69a9255b8e51f08fbfd3dc4146151d3
Doug Waldron <douglas.waldron@dnr.ga.gov>
2020-10-20 11:41:08 -0400

    Add EF migration for new Cabinet properties
    Includes scripts to update the new columns
    
    dotnet ef migrations add AddCabinetNumber -s FMS -p FMS.Infrastructure

--- FMS.Domain/Entities/Cabinet.cs
+++ FMS.Domain/Entities/Cabinet.cs
@@ -9,7 +9,9 @@ namespace FMS.Domain.Entities
         [Range(1, 999)]
         public int CabinetNumber { get; set; }
 
-        public string Name => string.Concat("C", CabinetNumber.ToString().PadLeft(3, '0'));
+        [StringLength(5)]
+        public string Name { get; set; }
+        // public string Name => string.Concat("C", CabinetNumber.ToString().PadLeft(3, '0'));
 
         /// <summary>
         /// The first/lowest file label in the cabinet. Used for determining appropriate cabinet when
--- FMS.Infrastructure/Migrations/20201020135314_AddCabinetNumber.Designer.cs
+++ FMS.Infrastructure/Migrations/20201020135314_AddCabinetNumber.Designer.cs
@@ -4,14 +4,16 @@ using FMS.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FMS.Infrastructure.Migrations
 {
     [DbContext(typeof(FmsDbContext))]
-    partial class FmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201020135314_AddCabinetNumber")]
+    partial class AddCabinetNumber
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
