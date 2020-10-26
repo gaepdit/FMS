0bc32259ddc4d6cd7890c3fc054d92aeeb30ba06
Tom Karasch <tom.karasch@dnr.ga.gov>
2020-10-26 18:00:39 -0400

    Changed Facility Type seed data to match Program Request

--- FMS.Infrastructure/SeedData/FacilityTypeSeedData.cs
+++ FMS.Infrastructure/SeedData/FacilityTypeSeedData.cs
@@ -15,12 +15,12 @@ namespace FMS.Infrastructure.SeedData
                     Id = new Guid("6F19934A-6AF2-438B-8858-03FA6AC4E78A"),
                     Active = true,
                     Code = 1,
-                    Name = "Generator"
+                    Name = "GEN"
                 },
                 new FacilityType
                 {
                     Id = new Guid("883B4581-2C73-429D-B198-82E352FD72F0"),
-                    Active = true,
+                    Active = false,
                     Code = 2,
                     Name = "GFAC"
                 },
@@ -48,7 +48,7 @@ namespace FMS.Infrastructure.SeedData
                 new FacilityType
                 {
                     Id = new Guid("4C30CECF-B53E-4D09-B919-A9E07E4E9782"),
-                    Active = true,
+                    Active = false,
                     Code = 6,
                     Name = "PAF"
                 },
@@ -57,26 +57,26 @@ namespace FMS.Infrastructure.SeedData
                     Id = new Guid("83E3005C-BD7C-4E52-918E-E1166F9483CC"),
                     Active = true,
                     Code = 7,
-                    Name = "HSI/HSRA"
+                    Name = "HSI"
                 },
                 new FacilityType
                 {
                     Id = new Guid("3FE54579-1762-4A77-9AD7-9D185E000A79"),
                     Active = true,
                     Code = 8,
-                    Name = "RCRA-non-gen TSD"
+                    Name = "TSD/CA"
                 },
                 new FacilityType
                 {
                     Id = new Guid("C8F61579-46F8-47FD-B7C4-FEB17F26384B"),
                     Active = true,
                     Code = 9,
-                    Name = "Brownfield"
+                    Name = "BROWN"
                 },
                 new FacilityType
                 {
                     Id = new Guid("8671519B-0544-43A3-A1E3-0CC82BD4028F"),
-                    Active = true,
+                    Active = false,
                     Code = 10,
                     Name = "PASI"
                 },
