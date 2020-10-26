850a5de406816ae4b16dd4313d8f1dfd71e67388
Tom Karasch <tom.karasch@dnr.ga.gov>
2020-10-26 17:51:40 -0400

    Un-comment facility types to allow data migration

--- FMS.Infrastructure/SeedData/FacilityTypeSeedData.cs
+++ FMS.Infrastructure/SeedData/FacilityTypeSeedData.cs
@@ -17,13 +17,13 @@ namespace FMS.Infrastructure.SeedData
                     Code = 1,
                     Name = "Generator"
                 },
-                //new FacilityType
-                //{
-                //    Id = new Guid("883B4581-2C73-429D-B198-82E352FD72F0"),
-                //    Active = true,
-                //    Code = 2,
-                //    Name = "GFAC"
-                //},
+                new FacilityType
+                {
+                    Id = new Guid("883B4581-2C73-429D-B198-82E352FD72F0"),
+                    Active = true,
+                    Code = 2,
+                    Name = "GFAC"
+                },
                 new FacilityType
                 {
                     Id = new Guid("1E51E549-9F79-42CE-8AA1-099A76E41BFC"),
@@ -31,13 +31,13 @@ namespace FMS.Infrastructure.SeedData
                     Code = 3,
                     Name = "NPL"
                 },
-                //new FacilityType
-                //{
-                //    Id = new Guid("F96E2355-12EE-4027-961C-25D87489A60A"),
-                //    Active = true,
-                //    Code = 4,
-                //    Name = "DOD"
-                //},
+                new FacilityType
+                {
+                    Id = new Guid("F96E2355-12EE-4027-961C-25D87489A60A"),
+                    Active = true,
+                    Code = 4,
+                    Name = "DOD"
+                },
                 new FacilityType
                 {
                     Id = new Guid("3FE94D7D-563E-4CA1-A094-BB6E217990D2"),
@@ -45,13 +45,13 @@ namespace FMS.Infrastructure.SeedData
                     Code = 5,
                     Name = "VRP"
                 },
-                //new FacilityType
-                //{
-                //    Id = new Guid("4C30CECF-B53E-4D09-B919-A9E07E4E9782"),
-                //    Active = true,
-                //    Code = 6,
-                //    Name = "PAF"
-                //},
+                new FacilityType
+                {
+                    Id = new Guid("4C30CECF-B53E-4D09-B919-A9E07E4E9782"),
+                    Active = true,
+                    Code = 6,
+                    Name = "PAF"
+                },
                 new FacilityType
                 {
                     Id = new Guid("83E3005C-BD7C-4E52-918E-E1166F9483CC"),
@@ -73,13 +73,13 @@ namespace FMS.Infrastructure.SeedData
                     Code = 9,
                     Name = "Brownfield"
                 },
-                //new FacilityType
-                //{
-                //    Id = new Guid("8671519B-0544-43A3-A1E3-0CC82BD4028F"),
-                //    Active = true,
-                //    Code = 10,
-                //    Name = "PASI"
-                //},
+                new FacilityType
+                {
+                    Id = new Guid("8671519B-0544-43A3-A1E3-0CC82BD4028F"),
+                    Active = true,
+                    Code = 10,
+                    Name = "PASI"
+                },
                 new FacilityType
                 {
                     Id = new Guid("44DD76AD-3637-47AA-B4DC-A19423B7EFBF"),
