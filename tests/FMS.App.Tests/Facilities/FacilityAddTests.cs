dc2ad87fd7d174a8c5671fe14b33db2aea102378
Doug Waldron <douglas.waldron@dnr.ga.gov>
2020-11-02 16:22:57 -0500

    Add unit tests for new facility confirmation page

--- tests/FMS.App.Tests/Facilities/FacilityAddTests.cs
+++ tests/FMS.App.Tests/Facilities/FacilityAddTests.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Moq;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 using Xunit.Extensions.AssertExtensions;
@@ -27,7 +28,80 @@ namespace FMS.App.Tests.Facilities
         }
 
         [Fact]
-        public async Task OnPost_IfValidModel_ReturnsDetailsPage()
+        public async Task OnPost_ValidModel_NoNearbyFacilities_ReturnsDetailsPage()
+        {
+            // ReSharper disable once CollectionNeverUpdated.Local
+            var nearbyFacilities = new List<FacilityMapSummaryDto>();
+            var newId = Guid.NewGuid();
+            var mockRepo = new Mock<IFacilityRepository>();
+            mockRepo.Setup(l => l.CreateFacilityAsync(It.IsAny<FacilityCreateDto>()))
+                .ReturnsAsync(newId);
+            mockRepo.Setup(l => l.GetFacilityListAsync(It.IsAny<FacilityMapSpec>()))
+                .ReturnsAsync(nearbyFacilities);
+
+            var mockSelectListHelper = new Mock<ISelectListHelper>();
+            var pageModel = new Pages.Facilities.AddModel(mockRepo.Object, mockSelectListHelper.Object)
+            {
+                Facility = new FacilityCreateDto {State = "Georgia"}
+            };
+
+            var result = await pageModel.OnPostAsync().ConfigureAwait(false);
+
+            result.Should().BeOfType<RedirectToPageResult>();
+            pageModel.ModelState.IsValid.ShouldBeTrue();
+            pageModel.ConfirmFacility.ShouldBeFalse();
+            pageModel.NearbyFacilities.Should().BeEquivalentTo(nearbyFacilities);
+            ((RedirectToPageResult) result).PageName.Should().Be("./Details");
+            ((RedirectToPageResult) result).RouteValues["id"].Should().Be(newId);
+        }
+
+        [Fact]
+        public async Task OnPost_ValidModel_NearbyFacilities_ReturnsConfirmationPage()
+        {
+            var nearbyFacilities = new List<FacilityMapSummaryDto>()
+            {
+                new FacilityMapSummaryDto()
+                {
+                    Id = Guid.Empty,
+                    FacilityNumber = "test",
+                }
+            };
+
+            var mockRepo = new Mock<IFacilityRepository>();
+            mockRepo.Setup(l => l.GetFacilityListAsync(It.IsAny<FacilityMapSpec>()))
+                .ReturnsAsync(nearbyFacilities);
+
+            var mockSelectListHelper = new Mock<ISelectListHelper>();
+            var pageModel = new Pages.Facilities.AddModel(mockRepo.Object, mockSelectListHelper.Object)
+            {
+                Facility = new FacilityCreateDto {State = "Georgia"}
+            };
+
+            var result = await pageModel.OnPostAsync().ConfigureAwait(false);
+
+            result.Should().BeOfType<PageResult>();
+            pageModel.ModelState.IsValid.ShouldBeTrue();
+            pageModel.ConfirmFacility.ShouldBeTrue();
+            pageModel.NearbyFacilities.Should().BeEquivalentTo(nearbyFacilities);
+        }
+
+        [Fact]
+        public async Task OnPost_InvalidModel_ReturnsPageWithInvalidModelState()
+        {
+            var mockRepo = new Mock<IFacilityRepository>();
+            var mockSelectListHelper = new Mock<ISelectListHelper>();
+            var pageModel = new Pages.Facilities.AddModel(mockRepo.Object, mockSelectListHelper.Object);
+            pageModel.ModelState.AddModelError("Error", "Sample error description");
+
+            var result = await pageModel.OnPostAsync().ConfigureAwait(false);
+
+            result.Should().BeOfType<PageResult>();
+            pageModel.ConfirmFacility.ShouldBeFalse();
+            pageModel.ModelState.IsValid.ShouldBeFalse();
+        }
+
+        [Fact]
+        public async Task OnPostConfirm_ValidModel_ReturnsDetailsPage()
         {
             var newId = Guid.NewGuid();
             var mockRepo = new Mock<IFacilityRepository>();
@@ -43,25 +117,27 @@ namespace FMS.App.Tests.Facilities
                 Facility = new FacilityCreateDto {State = "Georgia"}
             };
 
-            var result = await pageModel.OnPostAsync().ConfigureAwait(false);
+            var result = await pageModel.OnPostConfirmAsync().ConfigureAwait(false);
 
             result.Should().BeOfType<RedirectToPageResult>();
             pageModel.ModelState.IsValid.ShouldBeTrue();
+            pageModel.ConfirmFacility.ShouldBeFalse();
             ((RedirectToPageResult) result).PageName.Should().Be("./Details");
             ((RedirectToPageResult) result).RouteValues["id"].Should().Be(newId);
         }
 
         [Fact]
-        public async Task OnPost_IfInvalidModel_ReturnsPageWithInvalidModelState()
+        public async Task OnPostConfirm_InvalidModel_ReturnsPageWithInvalidModelState()
         {
             var mockRepo = new Mock<IFacilityRepository>();
             var mockSelectListHelper = new Mock<ISelectListHelper>();
             var pageModel = new Pages.Facilities.AddModel(mockRepo.Object, mockSelectListHelper.Object);
             pageModel.ModelState.AddModelError("Error", "Sample error description");
 
-            var result = await pageModel.OnPostAsync().ConfigureAwait(false);
+            var result = await pageModel.OnPostConfirmAsync().ConfigureAwait(false);
 
             result.Should().BeOfType<PageResult>();
+            pageModel.ConfirmFacility.ShouldBeFalse();
             pageModel.ModelState.IsValid.ShouldBeFalse();
         }
     }
