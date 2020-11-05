gaepdit/FMS

commit d6fffa2322bf9f74864c5fc863ec6ce6108d48ce

Add audit properties to database entities

FMS.Infrastructure/Contexts/FmsDbContext.cs
@@ -1,18 +1,27 @@
-﻿using FMS.Domain.Data;
-using FMS.Domain.Entities;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FMS.Domain.Data;
 using FMS.Domain.Dto;
+using FMS.Domain.Entities;
 using FMS.Domain.Entities.Users;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
+
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 
 namespace FMS.Infrastructure.Contexts
 {
     public class FmsDbContext : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>
     {
-        public FmsDbContext(DbContextOptions<FmsDbContext> options) : base(options) { }
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public FmsDbContext(DbContextOptions<FmsDbContext> options,
+            IHttpContextAccessor httpContextAccessor) : base(options) =>
+            _httpContextAccessor = httpContextAccessor;
 
         // App entities
         public DbSet<BudgetCode> BudgetCodes { get; set; }
@@ -41,7 +50,7 @@ namespace FMS.Infrastructure.Contexts
             base.OnModelCreating(builder ?? throw new ArgumentNullException(nameof(builder)));
 
             // Configure many-to-many relationships
-            builder.Entity<CabinetFile>().HasKey(e => new { e.CabinetId, e.FileId });
+            builder.Entity<CabinetFile>().HasKey(e => new {e.CabinetId, e.FileId});
 
             // Unique indexes
             builder.Entity<File>().HasIndex(e => e.FileLabel).IsUnique();
@@ -59,6 +68,55 @@ namespace FMS.Infrastructure.Contexts
 
             // Data
             builder.Entity<County>().HasData(Data.Counties);
+
+            // Auditing
+            foreach (var entityType in builder.Model.GetEntityTypes())
+            {
+                builder.Entity(entityType.ClrType).Property<DateTimeOffset?>(AuditProperties.InsertDateTime);
+                builder.Entity(entityType.ClrType).Property<DateTimeOffset?>(AuditProperties.UpdateDateTime);
+                builder.Entity(entityType.ClrType).Property<string>(AuditProperties.InsertUser);
+                builder.Entity(entityType.ClrType).Property<string>(AuditProperties.UpdateUser);
+            }
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            SetAuditProperties();
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public override int SaveChanges()
+        {
+            SetAuditProperties();
+            return base.SaveChanges();
+        }
+
+        private void SetAuditProperties()
+        {
+            var currentUser = _httpContextAccessor?.HttpContext?.User?.Identity?.Name;
+
+            var entries = ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Added || e.State == EntityState.Modified);
+
+            foreach (var entry in entries)
+            {
+                entry.Property(AuditProperties.UpdateDateTime).CurrentValue = DateTimeOffset.Now;
+                entry.Property(AuditProperties.UpdateUser).CurrentValue = currentUser;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Property(AuditProperties.InsertDateTime).CurrentValue = DateTimeOffset.Now;
+                    entry.Property(AuditProperties.InsertUser).CurrentValue = currentUser;
+                }
+            }
+        }
+
+        private static class AuditProperties
+        {
+            public const string InsertDateTime = "InsertDateTime";
+            public const string UpdateDateTime = "UpdateDateTime";
+            public const string InsertUser = "InsertUser";
+            public const string UpdateUser = "UpdateUser";
         }
     }
 }

FMS.Infrastructure/Migrations/20201104165409_AddAuditingProperties.Designer.cs
@@ -4,14 +4,16 @@ using FMS.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FMS.Infrastructure.Migrations
 {
     [DbContext(typeof(FmsDbContext))]
-    partial class FmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201104165409_AddAuditingProperties")]
+    partial class AddAuditingProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

tests/TestHelpers/RepositoryHelper.cs
@@ -14,7 +14,7 @@ namespace TestHelpers
 
         public RepositoryHelper()
         {
-            var context = new FmsDbContext(_options);
+            var context = new FmsDbContext(_options, default);
 
             context.Database.EnsureCreated();
             context.SeedData();
@@ -25,9 +25,9 @@ namespace TestHelpers
         }
 
         public FacilityRepository GetFacilityRepository() =>
-            new FacilityRepository(new FmsDbContext(_options), GetFileRepository());
+            new FacilityRepository(new FmsDbContext(_options, default), GetFileRepository());
 
         public FileRepository GetFileRepository() =>
-            new FileRepository(new FmsDbContext(_options));
+            new FileRepository(new FmsDbContext(_options, default));
     }
 }

tests/TestHelpers/SimpleRepository/SimpleRepositoryHelper.cs
@@ -12,7 +12,7 @@ namespace TestHelpers.SimpleRepository
 
         public SimpleRepositoryHelper()
         {
-            var context = new FmsDbContext(_options);
+            var context = new FmsDbContext(_options, default);
             context.Database.EnsureCreated();
             context.Files.AddRange(SimpleRepositoryData.Files);
             context.Facilities.AddRange(SimpleRepositoryData.Facilities);
@@ -23,15 +23,15 @@ namespace TestHelpers.SimpleRepository
         }
 
         public IFacilityRepository GetFacilityRepository() =>
-            new FacilityRepository(new FmsDbContext(_options), GetFileRepository());
+            new FacilityRepository(new FmsDbContext(_options, default), GetFileRepository());
 
         public IFileRepository GetFileRepository() =>
-            new FileRepository(new FmsDbContext(_options));
+            new FileRepository(new FmsDbContext(_options, default));
 
         public IItemsListRepository GetItemsListRepository() =>
-            new ItemsListRepository(new FmsDbContext(_options));
+            new ItemsListRepository(new FmsDbContext(_options, default));
 
         public ICabinetRepository GetCabinetRepository() =>
-            new CabinetRepository(new FmsDbContext(_options));
+            new CabinetRepository(new FmsDbContext(_options, default));
     }
 }

commit 8c5af899b29db97e37bebf267351633ca810f98b

FIx bug in location search
Location search entity `FacilityMapSummaryDto`
(`FacilityList` table) should not get auditing properties

Bug introduced in d6fffa23

FMS.Infrastructure/Contexts/FmsDbContext.cs
@@ -23,7 +23,7 @@ namespace FMS.Infrastructure.Contexts
             IHttpContextAccessor httpContextAccessor) : base(options) =>
             _httpContextAccessor = httpContextAccessor;
 
-        // App entities
+        // App entity tables
         public DbSet<BudgetCode> BudgetCodes { get; set; }
         public DbSet<ComplianceOfficer> ComplianceOfficers { get; set; }
         public DbSet<Facility> Facilities { get; set; }
@@ -35,14 +35,15 @@ namespace FMS.Infrastructure.Contexts
         public DbSet<RetentionRecord> RetentionRecords { get; set; }
         public DbSet<CabinetFile> CabinetFileJoin { get; set; }
 
-        // The "Counties" entity is only used to add a County table and data to the database for 
-        // database-side use. Counties are stored in memory and never accessed from the database,
-        // but other entities store County Id as a foreign key.
+        // The "Counties" table is only used to add County data to the database for database-side use.
+        // Counties are stored in memory and never accessed from the database, but other entities
+        // store County Id as a foreign key.
         // ReSharper disable once UnusedMember.Global
         public DbSet<County> Counties { get; set; }
 
-        // The "FacilityList" entity is only used for retrieving results from the [dbo].[getNearbyFacilities]
-        // stored procedure. (This should not be needed in .NET Core 5.)
+        // The "FacilityList" table is only used for retrieving "FacilityMapSummaryDto" results from
+        // the [dbo].[getNearbyFacilities] stored procedure.
+        // (This should not be needed in .NET Core 5.)
         public DbSet<FacilityMapSummaryDto> FacilityList { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
@@ -70,8 +71,12 @@ namespace FMS.Infrastructure.Contexts
             builder.Entity<County>().HasData(Data.Counties);
 
             // Auditing
-            foreach (var entityType in builder.Model.GetEntityTypes())
+            var entityTypes = builder.Model.GetEntityTypes();
+            foreach (var entityType in entityTypes)
             {
+                // Skip the "FacilityList" table
+                if (entityType.ClrType.Name == nameof(FacilityMapSummaryDto)) continue;
+                // Add auditing properties to all other entity tables
                 builder.Entity(entityType.ClrType).Property<DateTimeOffset?>(AuditProperties.InsertDateTime);
                 builder.Entity(entityType.ClrType).Property<DateTimeOffset?>(AuditProperties.UpdateDateTime);
                 builder.Entity(entityType.ClrType).Property<string>(AuditProperties.InsertUser);

FMS.Infrastructure/Migrations/20201105135603_FixAuditingProperties.Designer.cs
@@ -4,14 +4,16 @@ using FMS.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FMS.Infrastructure.Migrations
 {
     [DbContext(typeof(FmsDbContext))]
-    partial class FmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201105135603_FixAuditingProperties")]
+    partial class FixAuditingProperties
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

FMS.Infrastructure/Migrations/20201105135603_FixAuditingProperties.cs
@@ -1,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FMS.Infrastructure.Migrations
+{
+    public partial class FixAuditingProperties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InsertDateTime",
+                table: "FacilityList");
+
+            migrationBuilder.DropColumn(
+                name: "InsertUser",
+                table: "FacilityList");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateDateTime",
+                table: "FacilityList");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateUser",
+                table: "FacilityList");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "InsertDateTime",
+                table: "FacilityList",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InsertUser",
+                table: "FacilityList",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "UpdateDateTime",
+                table: "FacilityList",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdateUser",
+                table: "FacilityList",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}