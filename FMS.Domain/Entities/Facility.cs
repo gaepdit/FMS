3691a8c6c454ee1d4673c73c5a39bdc81d608cd2
Doug Waldron <douglas.waldron@dnr.ga.gov>
2020-11-09 10:31:58 -0500

    Remove virtual keyword from EF navigation properties
    `virtual` is not needed on navigation properties 
    unless lazy loading support is desired.

--- FMS.Domain/Entities/Facility.cs
+++ FMS.Domain/Entities/Facility.cs
@@ -34,29 +34,29 @@ namespace FMS.Domain.Entities
 
         // File label and Cabinet where this Facility is located
         public Guid FileId { get; set; }
-        public virtual File File { get; set; }
+        public File File { get; set; }
 
         // Type of Facility
         public Guid? FacilityTypeId { get; set; }
-        public virtual FacilityType FacilityType { get; set; }
+        public FacilityType FacilityType { get; set; }
 
         // Unit overseeing this Facility
         public Guid? OrganizationalUnitId { get; set; }
-        public virtual OrganizationalUnit OrganizationalUnit { get; set; }
+        public OrganizationalUnit OrganizationalUnit { get; set; }
 
         // Program Budget for this Facility
         public Guid? BudgetCodeId { get; set; }
-        public virtual BudgetCode BudgetCode { get; set; }
+        public BudgetCode BudgetCode { get; set; }
 
         // Facility Name
         public string Name { get; set; }
 
         // Compliance Officer assigned to this Facility
         public Guid? ComplianceOfficerId { get; set; }
-        public virtual ComplianceOfficer ComplianceOfficer { get; set; }
+        public ComplianceOfficer ComplianceOfficer { get; set; }
 
         public Guid? FacilityStatusId { get; set; }
-        public virtual FacilityStatus FacilityStatus { get; set; }
+        public FacilityStatus FacilityStatus { get; set; }
 
         // Location description distinct from mailing address
         public string Location { get; set; }
@@ -83,7 +83,7 @@ namespace FMS.Domain.Entities
 
         // County for use to simplify queries, ease searching, etc.
         public int CountyId { get; set; }
-        public virtual County County { get; set; }
+        public County County { get; set; }
 
         // List of retention records for this Facility
         public ICollection<RetentionRecord> RetentionRecords { get; set; }
