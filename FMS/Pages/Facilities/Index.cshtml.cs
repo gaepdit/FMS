4dff5f31539a940331066f1b15a12674c968a491
Tom Karasch <tom.karasch@dnr.ga.gov>
2022-02-09 14:56:07 -0500

    WIP Report Error
    Changes to bypass Reports Page

--- FMS/Pages/Facilities/Index.cshtml.cs
+++ FMS/Pages/Facilities/Index.cshtml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using CsvHelper.Configuration;
 using FMS.Domain.Data;
 using FMS.Domain.Dto;
 using FMS.Domain.Dto.PaginatedList;
@@ -20,6 +23,9 @@ namespace FMS.Pages.Facilities
 
         // List of facilities resulting from the search
         public IPaginatedResult FacilityList { get; private set; }
+
+        // Detailed Facility List to go to a report
+        public IReadOnlyList<FacilityDetailDto> FacilityReportList { get; private set; }
 
         // Shows results section after searching
         public bool ShowResults { get; private set; }
@@ -58,9 +64,12 @@ namespace FMS.Pages.Facilities
             return Page();
         }
 
-        public IActionResult OnPost(FacilitySpec spec)
+        public async Task<IActionResult> OnPostAsync()
         {
-            return RedirectToPage("../Reports/Index", spec);
+            FacilityReportList = await _repository.GetFacilityDetailListAsync(Spec);
+            return File(await FacilityReportList.GetCsvByteArrayAsync<FacilityReportMap>(), "text/csv",
+                $"FMS_export_{DateTime.Now:yyyy-MM-dd.HH-mm-ss.FFF}.csv");
+            //return RedirectToPage("../Reports/Index", spec);
         }
 
         private async Task PopulateSelectsAsync()
@@ -70,6 +79,29 @@ namespace FMS.Pages.Facilities
             FacilityStatuses = await _listHelper.FacilityStatusesSelectListAsync();
             FacilityTypes = await _listHelper.FacilityTypesSelectListAsync();
             OrganizationalUnits = await _listHelper.OrganizationalUnitsSelectListAsync();
+        }
+
+        private sealed class FacilityReportMap : ClassMap<FacilityDetailDto>
+        {
+            public FacilityReportMap()
+            {
+                Map(m => m.FacilityNumber).Index(0).Name("Facility Number");
+                Map(m => m.FileLabel).Index(1).Name("File Label");
+                Map(m => m.Name).Index(2).Name("Facility Name");
+                Map(m => m.Address).Index(3).Name("Street Address");
+                Map(m => m.City).Index(4).Name("City");
+                Map(m => m.County.Name).Index(5).Name("County");
+                Map(m => m.State).Index(6).Name("State");
+                Map(m => m.PostalCode).Index(7).Name("ZIP Code");
+                Map(m => m.Location).Index(8).Name("Location Description");
+                Map(m => m.FacilityType.Name).Index(9).Name("Type/Environmental Interest");
+                Map(m => m.ComplianceOfficer.Name).Index(10).Name("Compliance Officer");
+                Map(m => m.OrganizationalUnit.Name).Index(11).Name("Organizational Unit");
+                Map(m => m.BudgetCode.Name).Index(12).Name("Budget Code");
+                Map(m => m.FacilityStatus.Name).Index(13).Name("Facility Status");
+                Map(m => m.CabinetsToString).Index(14).Name("Cabinets");
+                Map(m => m.RetentionRecordsToString).Index(15).Name("Retention Records");
+            }
         }
     }
 }
