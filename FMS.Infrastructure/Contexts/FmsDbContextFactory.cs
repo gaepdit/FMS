03c40a163b08333180a072f0e31ed5c3586505c3
Doug Waldron <douglas.waldron@dnr.ga.gov>
2023-02-17 13:32:04 -0500

    Fix the Entity Framework migration commands

---
+++ FMS.Infrastructure/Contexts/FmsDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FMS.Infrastructure.Contexts
+{
+    /// <summary>
+    /// Facilitates some EF Core Tools commands. See "Design-time DbContext Creation":
+    /// https://docs.microsoft.com/en-us/ef/core/cli/dbcontext-creation?tabs=dotnet-core-cli#from-a-design-time-factory
+    /// </summary>
+    [UsedImplicitly]
+    public class FmsDbContextFactory : IDesignTimeDbContextFactory<FmsDbContext>
+    {
+        public FmsDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FmsDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=fms-temp;");
+            return new FmsDbContext(optionsBuilder.Options, null);
+        }
+    }
+}
