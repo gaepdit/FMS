854fb57f255c2de6fbb6db08146c7c1098963cfe
Tom Karasch <tom.karasch@dnr.ga.gov>
2024-03-05 16:05:18 -0500

    EF Update SP Nearby Facilities

---
+++ FMS.Infrastructure/Migrations/20240305210424_UpdateSPnearbyFacilities2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FMS.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSPnearbyFacilities2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
