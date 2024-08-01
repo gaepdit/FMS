18eb0a462f957721b0dbc2b0d4f203afe9a866c1
Caleb Nguyen <99635150+YoyoCaleb@users.noreply.github.com>
2024-08-01 07:58:13 -0400

    Add unit tests for user domain validation (#417)
    * Moved UserDomainValidation to FMS/Helpers. Added Unit Test for UserDomainValidation
    
    * Fix code style
    
    ---------
    
    Co-authored-by: Caleb Nguyen <caleb.nguyen@dnr.ga.gov>
    Co-authored-by: Doug Waldron <douglas.waldron@dnr.ga.gov>

--- FMS/Pages/Users/UserDomainValidation.cs
+++
@@ -1,8 +0,0 @@
-namespace FMS.Domain.Entities.Users
-{
-    public static class UserDomainValidation
-    {
-        public static bool IsValidEmailDomain(this string email) =>
-            email.EndsWith("@dnr.ga.gov", System.StringComparison.CurrentCultureIgnoreCase);
-    }
-}
