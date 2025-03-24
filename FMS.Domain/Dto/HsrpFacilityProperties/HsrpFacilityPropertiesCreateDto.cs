4ac51ce27a82bf0aee9e3aa815df4ee5c67b1da4
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-03-24 19:23:01 -0400

    HSRP Properties WIP

---
+++ FMS.Domain/Dto/HsrpFacilityProperties/HsrpFacilityPropertiesCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.Domain.Dto
+{
+    public class HsrpFacilityPropertiesCreateDto
+    {
+        public Guid FacilityId { get; set; } = Guid.Empty;
+
+        public DateTime DateListed { get; set; }
+
+        public string AdditionalOrgUnit { get; set; }
+
+        public string Geologist { get; set; }
+
+        public DateTime VRPDate { get; set; }
+
+        public DateTime BrownfieldDate { get; set; }
+    }
+}
---
+++ FMS.Domain/Entities/HSRPFacilityProperties.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Base;
+
+namespace FMS.Domain.Entities
+{
+    public class HsrpFacilityProperties : BaseActiveModel
+    {
+        public HsrpFacilityProperties() { }
+
+        public HsrpFacilityProperties(Guid facilityId, HsrpFacilityPropertiesCreateDto Hfpcd) 
+        {
+            FacilityId = facilityId;
+            DateListed = Hfpcd?.DateListed;
+            AdditionalOrgUnit = Hfpcd?.AdditionalOrgUnit;
+            Geologist = Hfpcd?.Geologist;
+            VRPDate = Hfpcd?.RPDate;
+            BrownfieldDate = Hfpcd?.BrownfieldDate;
+        }
+        public Guid FacilityId { get; set; } = Guid.Empty;
+        
+        public DateTime DateListed {  get; set; }
+
+        public string AdditionalOrgUnit { get; set; }
+
+        public string Geologist { get; set; }
+
+        public DateTime VRPDate { get; set; }
+
+        public DateTime BrownfieldDate { get; set; }
+    }
+}
