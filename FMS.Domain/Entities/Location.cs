8a552c701128cd2c1e6498f1232bb78607350dad
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-08 09:52:47 -0400

    WIP on Location and Parcel Entities

---
+++ FMS.Domain/Entities/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Base;
+
+namespace FMS.Domain.Entities
+{
+    public class Location : BaseActiveModel
+    {
+        public Location() { }
+    }
+}
