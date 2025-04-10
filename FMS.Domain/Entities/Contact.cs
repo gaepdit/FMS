9be78fbeed9c3852e050ed1a06bc3bf968cdbc27
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-10 13:04:42 -0400

    Contact Entity WIP

---
+++ FMS.Domain/Entities/Contact.cs
@@ -0,0 +1,41 @@
+﻿using FMS.Domain.Dto;
+using FMS.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.Domain.Entities
+{
+    public class Contact : BaseActiveModel
+    {
+        public Contact() { }
+        public Contact(ContactEditDto contact)
+        {
+            GivenName = contact.GivenName;
+            FamilyName = contact.FamilyName;
+            Title = contact.Title;
+            Type = contact.Type;
+            Company = contact.Company;
+            Address = contact.Address;
+            City = contact.City;
+            State = contact.State;
+            PostalCode = contact.PostalCode;
+            Email = contact.Email;
+            Status = contact.Status;
+        }
+        public string GivenName { get; set; }
+        public string FamilyName { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public string Company { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string PostalCode { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+    }
+}
