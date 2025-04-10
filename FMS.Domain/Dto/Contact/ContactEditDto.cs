5644663b8b32d14f37697a58890b2c781acf5b79
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-10 16:21:25 -0400

    Adds Contact Title and Type creation DTOs
    Adds DTOs for creating Contact Title and Contact Type entities.
    
    These DTOs include properties for Title/Type, Description, IsActive status, and Created/Updated dates, with data annotations for validation and display.
    
    Removes unused usings in existing files.

--- FMS.Domain/Dto/Contact/ContactEditDto.cs
+++ FMS.Domain/Dto/Contact/ContactEditDto.cs
@@ -1,10 +1,5 @@
 ﻿using FMS.Domain.Entities;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FMS.Domain.Dto
 {
--- FMS.Domain/Entities/Contact.cs
+++ FMS.Domain/Entities/Contact.cs
@@ -1,11 +1,6 @@
 ﻿using FMS.Domain.Dto;
 using FMS.Domain.Entities.Base;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FMS.Domain.Entities
 {
