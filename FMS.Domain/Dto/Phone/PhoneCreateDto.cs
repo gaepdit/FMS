3da3b53e1ba6faafad1dc0a5aaa186cebe134775
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-10 17:19:43 -0400

    Adds Phone entity and DTO
    Adds the Phone entity and PhoneCreateDto for managing phone number information.
    This allows storing and transferring phone data within the application.

---
+++ FMS.Domain/Dto/Phone/PhoneCreateDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.Domain.Dto
+{
+    public class PhoneCreateDto
+    {
+        public Guid ContactId { get; set; }
+
+        [Display(Name = "Country Code")]
+        public int CountryCode { get; set; }
+
+        [Display(Name = "Area Code")]
+        public int AreaCode { get; set; }
+
+        [Display(Name = "Prefix")]
+        public int Prefix { get; set; }
+
+        [Display(Name = "Number")]
+        public int Number { get; set; }
+
+        [Display(Name = "Phone Type")]
+        public string Type { get; set; }
+    }
+}
---
+++ FMS.Domain/Entities/Phone.cs
@@ -0,0 +1,35 @@
+﻿using FMS.Domain.Dto;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.Domain.Entities
+{
+    public class Phone
+    {
+        public Phone() { }
+        public Phone(Guid id, PhoneCreateDto phoneCreateDto)
+        {
+            ContactId = id;
+            CountryCode = phoneCreateDto.CountryCode;
+            AreaCode = phoneCreateDto.AreaCode;
+            Prefix = phoneCreateDto.Prefix;
+            Number = phoneCreateDto.Number;
+            Type = phoneCreateDto.Type;
+        }
+        public Guid ContactId { get; set; }
+
+        public int CountryCode { get; set; }
+
+        public int AreaCode { get; set; }
+
+        public int Prefix { get; set; }
+
+        public int Number { get; set; }
+
+        public string Type { get; set; }
+    }
+}
