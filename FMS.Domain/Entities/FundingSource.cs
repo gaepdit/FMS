81c16fafa4030a14733e90cbb237f5380fb1d3b6
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-14 21:46:22 -0400

    Adds Chemical and Funding Source entities
    Adds the Chemical and Funding Source entities along with their corresponding Create DTOs.
    
    Includes validation attributes for required fields and data trimming for strings.

---
+++ FMS.Domain/Entities/FundingSource.cs
@@ -0,0 +1,23 @@
+﻿using FMS.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FMS.Domain.Dto;
+
+namespace FMS.Domain.Entities
+{
+    public class FundingSource : BaseActiveNamedModel
+    {
+        public FundingSource() { }
+        public FundingSource(FundingSourceCreateDto fundingSource)
+        {
+            Name = fundingSource.Name;
+        }
+        public void TrimAll()
+        {
+            Name = Name?.Trim();
+        }
+    }
+}
