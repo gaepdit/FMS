6bfd66a851bc43f45c0db24b9f113b464c121005
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-17 14:11:47 -0400

    Updates OnSiteScore entity
    Adds properties to the OnSiteScore entity.
    Removes unnecessary usings from DTO.

--- FMS.Domain/Entities/OnSiteScore.cs
+++ FMS.Domain/Entities/OnSiteScore.cs
@@ -29,16 +29,29 @@ namespace FMS.Domain.Entities
             E2 = onSiteScore.E2;
         }
         public Guid ScoreId { get; set; }
+
         public string OSScore { get; set; }
+
         public string A { get; set; }
+
         public string B { get; set; }
+
         public string C { get; set; }
+
         public string Description { get; set; }
+
         public string ChemName1D { get; set; }
+
         public string Other1D { get; set; }
+
         public string D2 { get; set; }
+
         public string D3 { get; set; }
+
         public string CASNO { get; set; }
+
         public string E1 { get; set; }
+
         public string E2 { get; set; }
     }
+}
