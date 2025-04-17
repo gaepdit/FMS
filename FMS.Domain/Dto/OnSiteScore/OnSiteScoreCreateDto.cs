ef4a6c98f81fef9ec31cff316e6794ed46d877f7
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-17 14:07:35 -0400

    Adds OnSiteScore entity and DTO
    Adds the OnSiteScore entity and related DTO for creating OnSiteScore objects.
    This allows for storing and transferring OnSiteScore data within the application.

---
+++ FMS.Domain/Dto/OnSiteScore/OnSiteScoreCreateDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FMS.Domain.Entities;
+
+namespace FMS.Domain.Dto
+{
+    public class OnSiteScoreCreateDto
+    {
+        public Guid ScoreId { get; set; }
+        public string OnSiteScore { get; set; }
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string ChemName1D { get; set; }
+        public string Other1D { get; set; }
+        public string D2 { get; set; }
+        public string D3 { get; set; }
+        public string Description { get; set; }
+        public string CASNO { get; set; }
+        public string E1 { get; set; }
+        public string E2 { get; set; }
+    }
+}
