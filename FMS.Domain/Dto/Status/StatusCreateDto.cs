be6bd724d1c9a1a707aa6ace98b71528e4a88fc5
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-21 13:45:24 -0400

    Adds Status entity and DTO WIP
    Adds the Status entity to represent different states or conditions within the system.
    Also introduces the StatusCreateDto for creating new Status entities.

---
+++ FMS.Domain/Dto/Status/StatusCreateDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FMS.Domain.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace FMS.Domain.Dto
+{
+    public class StatusCreateDto
+    {
+        // Name of the status
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        // Description of the status
+        [StringLength(200)]
+        public string Description { get; set; }
+        // Indicates if the status is retained
+        public bool IsRetained { get; set; }
+    }
+}
---
+++ FMS.Domain/Entities/Status.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using FMS.Domain.Entities.Base;
+using FMS.Domain.Dto;
+
+namespace FMS.Domain.Entities
+{
+    public class Status : BaseActiveModel
+    {
+        public Status() { }
+        public Status(StatusCreateDto newStatus)
+        {
+            Name = newStatus.Name;
+            Description = newStatus.Description;
+            IsRetained = newStatus.IsRetained;
+        }
+        // Name of the status
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        // Description of the status
+        [StringLength(200)]
+        public string Description { get; set; }
+        // Indicates if the status is retained
+        public bool IsRetained { get; set; }
+    }
+}
