0b8fd9ce8c9917e556e2df2ece5108d1f961836f
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-22 18:41:29 -0400

    Cleans up Event DTO and Entity files
    Removes unnecessary using statements from the EventCreateDto and Event entity files to improve code readability and maintainability.

--- FMS.Domain/Entities/Event.cs
+++ FMS.Domain/Entities/Event.cs
@@ -1,11 +1,6 @@
 ﻿using FMS.Domain.Entities.Base;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using FMS.Domain.Dto;
-using Microsoft.VisualBasic;
 
 namespace FMS.Domain.Entities
 {
