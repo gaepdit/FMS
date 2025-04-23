f82e10e322f15b4700c6abab08fe783315fb03d9
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-23 18:40:57 -0400

    Adds seed data files
    Adds initial seed data files for ActionTaken, AllowedActionTaken,
    and Chemical entities. These files provide a starting point for
    populating these entities in development environments.

---
+++ FMS/Platform/DevHelpers/SeedData/ActionTakenSeedData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using FMS.Domain.Entities;
+
+// ReSharper disable StringLiteralTypo
+
+namespace FMS.Platform.Extensions.DevHelpers.SeedData
+{
+    public static partial class SeedData
+    {
+        private static IEnumerable<ActionTaken> GetActionTakens()
+        {
+            return new List<ActionTaken>()
+            {
+                
+            };
+        }
+    }
+}
---
+++ FMS/Platform/DevHelpers/SeedData/ChemicalSeedData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using FMS.Domain.Entities;
+
+// ReSharper disable StringLiteralTypo
+
+namespace FMS.Platform.Extensions.DevHelpers.SeedData
+{
+    public static partial class SeedData
+    {
+        private static IEnumerable<Chemical> GetChemicals()
+        {
+            return new List<Chemical>()
+            {
+
+            };
+        }
+    }
+}
