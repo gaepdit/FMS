f163cc919a99e6370f988a50695a97eab2831a22
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-25 17:52:01 -0400

    Adds Overall Status entity and DTO
    Adds the OverallStatus entity with basic properties,
    including validation for the Name field. Also introduces
    the corresponding DTO for data transfer purposes.

---
+++ FMS.Domain/Dto/OverallStatus/OverallStatusCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FMS.Domain.Dto
+{
+    public class OverallStatusCreateDto
+    {
+        [Display(Name = "Overall Status")]
+        [Required(ErrorMessage = "Overall Status Name is required.")]
+        public string Name { get; set; }
+    }
+}
