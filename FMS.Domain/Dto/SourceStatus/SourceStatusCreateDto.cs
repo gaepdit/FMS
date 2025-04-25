7df2e688b9b38c4ede3230542153a54d51793a83
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-04-25 18:08:17 -0400

    Adds SourceStatus entity and DTO
    Adds the SourceStatus entity with basic properties,
    and its corresponding create DTO with validation.
    
    These new classes will support managing the status
    of a source within the application.

---
+++ FMS.Domain/Dto/SourceStatus/SourceStatusCreateDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FMS.Domain.Dto
+{
+    public class SourceStatusCreateDto
+    {
+        [Display(Name = "Source Status")]
+        [Required(ErrorMessage = "Source Status Name is required.")]
+        public string Name { get; set; }
+    }
+}
