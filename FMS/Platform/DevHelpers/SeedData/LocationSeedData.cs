66bb91dd82136b03641dfadfe4b88e770f65feef
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-05-06 17:16:00 -0400

    Replaces score with class for locations
    Updates the location entities and DTOs to use "Class" instead of "Score."
    Adds the Class domain with allowed values.
    This change aligns the data model with current requirements.

--- FMS/Platform/DevHelpers/SeedData/LocationSeedData.cs
+++ FMS/Platform/DevHelpers/SeedData/LocationSeedData.cs
@@ -17,42 +17,42 @@ namespace FMS.Platform.Extensions.DevHelpers.SeedData
                     Id = new Guid("533EBBD5-4144-41D5-9338-C3C5317C3E5E"),
                     Active = true,
                     FacilityId = new Guid(""),
-                    Score = "I",
+                    Class = "I",
                 },
                 new()
                 {
                     Id = new Guid("A2ACAA03-EEAA-4409-B16B-1759FB4314E3"),
                     Active = true,
                     FacilityId = new Guid(""),
-                    Score = "II",
+                    Class = "II",
                 },
                 new()
                 {
                     Id = new Guid("41CD50F4-353D-450A-AB4E-8BE3EAF27CA8"),
                     Active = true,
                     FacilityId = new Guid(""),
-                    Score = "III",
+                    Class = "III",
                 },
                 new()
                 {
                     Id = new Guid("77D2B98D-0C55-46EA-9583-B62BD934762C"),
                     Active = true,
                     FacilityId = new Guid(""),
-                    Score = "IV",
+                    Class = "IV",
                 },
                 new()
                 {
                     Id = new Guid("A99FF86A-2F97-48DB-830B-9C92F88DE681"),
                     Active = true,
                     FacilityId = new Guid(""),
-                    Score = "V",
+                    Class = "V",
                 },
                 new()
                 {
                     Id = new Guid("98B3C9ED-F747-48AE-8B92-497A35927E90"),
                     Active = true,
                     FacilityId = new Guid(""),
-                    Score = "ER",
+                    Class = "ER",
                 }
             };
         }
