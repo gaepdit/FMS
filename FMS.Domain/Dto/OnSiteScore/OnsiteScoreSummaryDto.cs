f70777743b98f499652f6f745368c0bfaa5d98e1
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-05-07 20:49:16 -0400

    Defines OnsiteScore DTOs
    Defines the OnsiteScoreEditDto and OnsiteScoreSummaryDto classes.
    
    These DTOs are used for transferring OnSiteScore data between different
    layers of the application. Includes properties representing various fields
    related to the on-site score, such as score values, descriptions, and
    chemical information.

--- FMS.Domain/Dto/OnSiteScore/OnsiteScoreSummaryDto.cs
+++ FMS.Domain/Dto/OnSiteScore/OnsiteScoreSummaryDto.cs
@@ -1,12 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using FMS.Domain.Entities;
 
-namespace FMS.Domain.Dto.OnSiteScore
+namespace FMS.Domain.Dto
 {
-    internal class OnsiteScoreSummaryDto
+    public class OnsiteScoreSummaryDto
     {
+        public OnsiteScoreSummaryDto() { }
+
+        public OnsiteScoreSummaryDto(OnSiteScore onSiteScore)
+        {
+            Id = onSiteScore.Id;
+            Active = onSiteScore.Active;
+            ScoreId = onSiteScore.ScoreId;
+            ScoreValue = onSiteScore.ScoreValue;
+            A = onSiteScore.A;
+            B = onSiteScore.B;
+            C = onSiteScore.C;
+            Description = onSiteScore.Description;
+            ChemName1D = onSiteScore.ChemName1D;
+            Other1D = onSiteScore.Other1D;
+            D2 = onSiteScore.D2;
+            D3 = onSiteScore.D3;
+            CASNO = onSiteScore.CASNO;
+            E1 = onSiteScore.E1;
+            E2 = onSiteScore.E2;
+        }
+
+        public Guid Id { get; set; }
+
+        public bool Active { get; set; }
+
+        [Required]
+        public Guid ScoreId { get; set; }
+
+        public string ScoreValue { get; set; }
+
+        public int A { get; set; }
+
+        public int B { get; set; }
+
+        public int C { get; set; }
+
+        public string Description { get; set; }
+
+        public string ChemName1D { get; set; }
+
+        public string Other1D { get; set; }
+
+        public int D2 { get; set; }
+
+        public int D3 { get; set; }
+
+        public Guid ChemicalId { get; set; }
+
+        public string CASNO { get; set; }
+
+        public int E1 { get; set; }
+
+        public int E2 { get; set; }
     }
 }
