35d1a2134b75b329318514ca099f76383616fbcb
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-05-08 13:59:43 -0400

    Adds onsite score repository interface
    Defines the interface for the onsite score repository.
    
    This interface outlines the methods for interacting with onsite score data,
    including checking existence, retrieval, creation, updating, and deletion.

---
+++ FMS.Domain/Repositories/IOnsiteScoreRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Base;
+using FMS.Domain.Entities;
+
+namespace FMS.Domain.Repositories
+{
+    public interface IOnsiteScoreRepository
+    {
+        Task<bool> OnsiteScoreExistsAsync(Guid id);
+        Task<OnsiteScoreEditDto> GetOnsiteScoreByIdAsync(Guid id);
+        Task<bool> AddOnsiteScoreAsync(OnSiteScoreCreateDto onSiteScore);
+        Task<bool> UpdateOnsiteScoreAsync(OnsiteScoreEditDto onSiteScore);
+        Task<bool> DeleteOnsiteScoreAsync(Guid id);
+    }
+}
