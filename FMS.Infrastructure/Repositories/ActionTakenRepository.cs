e51543a12091bd9b9aa9648eb90fdf1c851d445e
yoyocaleb <yoyocaleb1011@gmail.com>
2025-05-21 09:21:24 -0400

    Created ExistAsync and NameExistAsync

--- FMS.Infrastructure/Repositories/ActionTakenRepository.cs
+++ FMS.Infrastructure/Repositories/ActionTakenRepository.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FMS.Domain.Repositories;
+using FMS.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace FMS.Infrastructure.Repositories
 {
-    internal class ActionTakenRepository
+    internal class ActionTakenRepository : IActionTakenRepository
     {
+        public readonly FmsDbContext _context;
+        public Task<bool> ActionTakenExistAsync(Guid id) =>
+            _context.FacilityStatuses.AnyAsync(e => e.Id == id);
+        public Task<bool> ActionTakenNameExistAsync(string status, Guid? ignoreId = null) =>
+            _context.FacilityStatuses.AnyAsync(e =>
+                e.Status == status && (!ignoreId.HasValue || e.Id != ignoreId.Value));
     }
 }
