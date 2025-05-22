824178b87977bd7a3b482352cd500ffeea2ca68e
yoyocaleb <yoyocaleb1011@gmail.com>
2025-05-22 11:16:17 -0400

    implemented some of the interfaces functions

--- FMS.Infrastructure/Repositories/ActionTakenRepository.cs
+++ FMS.Infrastructure/Repositories/ActionTakenRepository.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using FMS.Domain.Dto;
 using FMS.Domain.Repositories;
+using FMS.Domain.Utils;
 using FMS.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,24 +14,53 @@ namespace FMS.Infrastructure.Repositories
     internal class ActionTakenRepository : IActionTakenRepository
     {
         public readonly FmsDbContext _context;
-        public FacilityStatusRepository(FmsDbContext context) => _context = context;
+        public ActionTakenRepository(FmsDbContext context) => _context = context;
         public Task<bool> ActionTakenExistAsync(Guid id) =>
-            _context.FacilityStatuses.AnyAsync(e => e.Id == id);
+            _context.ActionTaken.AnyAsync(e => e.Id == id);
         public Task<bool> ActionTakenNameExistAsync(string status, Guid? ignoreId = null) =>
-            _context.FacilityStatuses.AnyAsync(e =>
-                e.Status == status && (!ignoreId.HasValue || e.Id != ignoreId.Value));
+            _context.ActionTaken.AnyAsync(e =>
+                e.Name == status && (!ignoreId.HasValue || e.Id != ignoreId.Value));
 
         public async Task<ActionTakenEditDto> GetActionTakenAsync(Guid id)
         {
-            var actionTaken = await _context.
-        };
+            var actionTaken = await _context.ActionTaken.AsNoTracking().
+                SingleOrDefaultAsync(e => e.Id == id);
+            if (actionTaken == null)
+            {
+                return null;
+            }
+            return new ActionTakenEditDto(actionTaken);
+        }
 
-        public Task<IReadOnlyList<ActionTakenSummaryDto>> GetActionTakenListAsync;
+        public async Task<IReadOnlyList<ActionTakenSummaryDto>> GetActionTakenListAsync() =>
+            await _context.ActionTaken.AsNoTracking()
+                .OrderBy(e => e.Name)
+                .Select(e => new ActionTakenSummaryDto(e));
+                .ToListAsync();
 
-        public Task<Guid> CreateActionTakenAsync;
+        public Task<Guid> CreateActionTakenAsync(ActionTakenCreateDto actionTaken)
+        {
+            Prevent.Null(actionTaken, nameof(actionTaken));
+            Prevent.NullOrEmpty(actionTaken.Name, nameof(actionTaken.Name));
 
-        public Task UpdateActionTakenAsync;
+            return CreateActionTakenInternalAsync(actionTaken);
+        }
 
-        public Task UpdateActionTakenStatusAsync;
+        public Task UpdateActionTakenAsync(Guid id, ActionTakenEditDto actionTaken)
+        {
+            Prevent.NullOrEmpty(actionTaken.Name, nameof(actionTaken.Name));
+            return UpdateActionTakenAsync(id, actionTaken);
+        }
+
+        public async Task UpdateActionTakenStatusAsync(Guid id, bool active)
+        {
+            var actionTaken = await _context.ActionTaken.FindAsync(id);
+            if (actionTaken == null)
+            {
+                throw new ArgumentException("Action Taken ID not found");
+            }
+            actionTaken.Active = active;
+            await _context.SaveChangesAsync();
+        }
     }
 }
