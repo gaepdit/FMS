0d07b72bb2b6b8bb6bbb2b97f2a4303f2d4a0ce9
yoyocaleb <yoyocaleb1011@gmail.com>
2025-05-23 11:47:24 -0400

    implemented all methods

--- FMS.Infrastructure/Repositories/ActionTakenRepository.cs
+++ FMS.Infrastructure/Repositories/ActionTakenRepository.cs
@@ -10,10 +10,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FMS.Domain.Dto;
+using FMS.Domain.Entities;
 using FMS.Domain.Repositories;
 using FMS.Domain.Utils;
 using FMS.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace FMS.Infrastructure.Repositories
 {
