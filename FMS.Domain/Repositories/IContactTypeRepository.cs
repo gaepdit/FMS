8af69965be1927a02de79603da37978343487909
yoyocaleb <yoyocaleb1011@gmail.com>
2025-05-28 11:09:07 -0400

    added ContactType to dbset, created new repository

--- FMS.Domain/Repositories/IContactTypeRepository.cs
+++ FMS.Domain/Repositories/IContactTypeRepository.cs
@@ -8,15 +8,10 @@ namespace FMS.Domain.Repositories
     public interface IContactTypeRepository : IDisposable
     {
         Task<bool> ContactTypeExistsAsync(Guid id);
-
         Task<ContactTypeEditDto> GetContactTypeByIdAsync(Guid id);
-
         Task<IReadOnlyList<ContactTypeSummaryDto>> GetContactTypeListsAsync();
-
         Task<Guid> CreateContactTypeAsync(ContactTypeCreateDto contactType);
-
         Task UpdateContactTypeAsync(Guid Id, ContactTypeEditDto contactTypeUpdates);
-
         Task UpdateContactTypeStatusAsync(Guid id, bool active);
     }
 }
---
+++ FMS.Infrastructure/Repositories/ContactTypeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FMS.Domain.Dto;
+
+namespace FMS.Infrastructure.Repositories
+{
+    internal class ContactTypeRepository
+    {
+        public Task<bool> ContactTypeExistsAsync(Guid id) =>
+            ;
+        public async Task<ContactTypeEditDto> GetContactTypeByIdAsync(Guid id);
+        public async Task<IReadOnlyList<ContactTypeSummaryDto>> GetContactTypeListAsync();
+        public Task<Guid> CreateContactTypeAsync(ContactTypeCreateDto contactType);
+        public Task UpdateContactTypeAsync(Guid Id, ContactTypeEditDto contactTypeUpdate);
+        public async Task UpdateContactTypeStatusAsync(Guid id, bool active);
+    }
+}
