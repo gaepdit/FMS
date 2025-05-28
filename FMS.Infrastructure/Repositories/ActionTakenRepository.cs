cd404056780990a45345b3b494f8fd58cc55c6a2
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-05-28 19:32:34 -0400

    Makes ActionTakenRepository testable
    Changes the ActionTakenRepository to be public and its Dispose method to private to enable unit testing.
    
    Adds a new test file for ActionTakenRepository with basic setup and a placeholder test.

--- FMS.Infrastructure/Repositories/ActionTakenRepository.cs
+++ FMS.Infrastructure/Repositories/ActionTakenRepository.cs
@@ -97,7 +97,7 @@ namespace FMS.Infrastructure.Repositories
 
         private bool _disposedValue;
 
-        protected virtual void Dispose(bool disposing)
+        private void Dispose(bool disposing)
         {
             if (_disposedValue) return;
 
