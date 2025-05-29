1c3cc83ee1cb960224b1bb7e53336e46d831e084
yoyocaleb <yoyocaleb1011@gmail.com>
2025-05-29 10:08:15 -0400

    Implemented most methods

--- FMS.Infrastructure/Repositories/ContactTypeRepository.cs
+++ FMS.Infrastructure/Repositories/ContactTypeRepository.cs
@@ -1,20 +1,73 @@
-﻿using System;
+﻿using FMS.Domain.Dto;
+using FMS.Domain.Entities;
+using FMS.Domain.Repositories;
+using FMS.Domain.Utils;
+using FMS.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using FMS.Domain.Dto;
 
 namespace FMS.Infrastructure.Repositories
 {
-    internal class ContactTypeRepository
+    internal class ContactTypeRepository : IContactTypeRepository
     {
+        public readonly FmsDbContext _context;
+        public ContactTypeRepository(FmsDbContext context) => _context = context;
         public Task<bool> ContactTypeExistsAsync(Guid id) =>
-            ;
-        public async Task<ContactTypeEditDto> GetContactTypeByIdAsync(Guid id);
-        public async Task<IReadOnlyList<ContactTypeSummaryDto>> GetContactTypeListAsync();
-        public Task<Guid> CreateContactTypeAsync(ContactTypeCreateDto contactType);
-        public Task UpdateContactTypeAsync(Guid Id, ContactTypeEditDto contactTypeUpdate);
-        public async Task UpdateContactTypeStatusAsync(Guid id, bool active);
+            _context.ContactType.AnyAsync(e => e.Id == id);
+        public async Task<ContactTypeEditDto> GetContactTypeByIdAsync(Guid id)
+        {
+            var contactType = await _context.ContactType.AsNoTracking().
+                SingleOrDefaultAsync(e => e.Id == id);
+            if (contactType == null)
+            {
+                return null;
+            }
+            return new ContactTypeEditDto(contactType);
+        }
+        public async Task<IReadOnlyList<ContactTypeSummaryDto>> GetContactTypeListAsync() => await _context.ContactType.AsNoTracking()
+           .OrderBy(e => e.Name)
+           .Select(e => new ContactTypeSummaryDto(e))
+           .ToListAsync();
+        public Task<Guid> CreateContactTypeAsync(ContactTypeCreateDto contactType)
+        {
+            Prevent.Null(contactType, nameof(contactType));
+            Prevent.NullOrEmpty(contactType.Name, nameof(contactType.Name));
+
+            return CreateContactTypeInternalAsync(contactType);
+        }
+        private async Task<Guid> CreateContactTypeInternalAsync(ContactTypeCreateDto contactType)
+        {
+            var newCT = new ContactType(contactType);
+            await _context.ContactType.AddAsync(newCT);
+            await _context.SaveChangesAsync();
+
+            return newCT.Id;
+        }
+        public Task UpdateContactTypeAsync(Guid id, ContactTypeEditDto contactType)
+        {
+            Prevent.NullOrEmpty(contactType.Name, nameof(contactType.Name));
+            return UpdateContactTYpeInternalAsync(id, contactType);
+        }
+        private async Task<Guid> UpdateContactTYpeInternalAsync(Guid id, ContactTypeEditDto contactTypeUpdates)
+        {
+            var contactType = await _context.ContactType.FindAsync(id) ?? throw new ArgumentException("Contact Type ID not found.", nameof(id));
+
+            contactType.Name = contactTypeUpdates.Name;
+
+            await _context.SaveChangesAsync();
+
+            // Ensure all code paths return a value
+            return contactType.Id;
+        }
+        public async Task UpdateContactTypeStatusAsync(Guid id, bool active)
+        {
+            var contactType = await _context.ContactType.FindAsync(id)
+                ?? throw new ArgumentException("Contact Type ID not found");
+            contactType.Active = active;
+            await _context.SaveChangesAsync();
+        }
     }
 }
