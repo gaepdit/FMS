4814b2a24c15fa4f38e2047774aa22ad63171115
yoyocaleb <yoyocaleb1011@gmail.com>
2025-06-03 10:57:37 -0400

    Added first Test with setup

--- tests/FMS.Infrastructure.Tests/ContactTypeRepositoryTest.cs
+++ tests/FMS.Infrastructure.Tests/ContactTypeRepositoryTest.cs
@@ -18,5 +18,50 @@ namespace FMS.Infrastructure.Tests
     {
         private FmsDbContext _context;
         private ContactTypeRepository _repository;
+        private bool _disposed = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<FmsDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{Guid.NewGuid()}")
+                .Options;
+            var httpContextAccessor = Substitute.For<HttpContextAccessor>();
+            _context = new FmsDbContext(options, httpContextAccessor);
+            _repository = new ContactTypeRepository(_context);
+
+            _context.ContactType.Add(new ContactType
+            {
+                Id = Guid.NewGuid(),
+                Name = "Contact Type",
+                Active = true
+            });
+            _context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _context.Database.EnsureDeleted();
+                _context.Dispose();
+                _repository.Dispose();
+                _disposed = true;
+            }
+        }
+
+        //ContactTypeExistAsync
+        [Test]
+        public async Task ContactTypeExistAsync_ReturnTrue_ContactTypeExist()
+        {
+            var existingCT = await _context.ContactType.Select(ft => ft.Id).FirstAsync();
+            var results = await _repository.ContactTypeExistsAsync(existingCT);
+            results.Should().BeTrue();
+        }
     }
 }
