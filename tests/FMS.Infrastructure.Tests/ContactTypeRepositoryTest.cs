70e1f6ae79ed6a725388cb520b99ba558315a1ab
yoyocaleb <yoyocaleb1011@gmail.com>
2025-06-05 12:01:24 -0400

    added testing for create contact type

--- tests/FMS.Infrastructure.Tests/ContactTypeRepositoryTest.cs
+++ tests/FMS.Infrastructure.Tests/ContactTypeRepositoryTest.cs
@@ -10,6 +10,7 @@ using FMS.Domain.Entities;
 using FMS.Domain.Dto;
 using Microsoft.AspNetCore.Http;
 using FluentAssertions;
+using System.Collections.Generic;
 
 namespace FMS.Infrastructure.Tests
 {
@@ -61,7 +62,67 @@ namespace FMS.Infrastructure.Tests
         {
             var existingCT = await _context.ContactType.Select(ft => ft.Id).FirstAsync();
             var results = await _repository.ContactTypeExistsAsync(existingCT);
-            results.Should().BeTrue();
+            results.Should().BeTrue();      
         }
+        [Test]
+        public async Task ContactTypeExistAsync_ReturnFalse_ContactTypeDoesNotExist()
+        {
+            var nonExistingCT = Guid.NewGuid();
+            var results = await _repository.ContactTypeExistsAsync(nonExistingCT);
+            results.Should().BeFalse();
+        }
+
+        //GetContactTypeByIdAsync
+
+        //GetContactTypeListAsync
+        [Test]
+        public async Task GetContactTypeListAsync_ReturnsAllContactTypes()
+        {
+            var results = await _repository.GetContactTypeListAsync();
+            results.Should().NotBeNullOrEmpty();
+        }
+
+        //CreateContactTypeAsync
+        [Test]
+        public async Task CreateContactTypeAsync_CreateNewContactType_WhenDataIsValid()
+        {
+            var dto = new ContactTypeCreateDto { Name = "UniqueName" };
+
+            var newId = await _repository.CreateContactTypeAsync(dto);
+            var createdContactType = await _context.ContactType.FindAsync(newId);
+
+            createdContactType.Should().NotBeNull();
+            createdContactType.Name.Should().Be("UniqueName");
+        }
+        [Test]
+        public void CreateContactTypeAsync_ThrowsArgumentException_WhereNameAlreadyExist()
+        {
+            var existingContactType = new ContactType { Id = Guid.NewGuid(), Name = "DuplicateName" };
+            _context.ContactType.Add(existingContactType);
+            _context.SaveChanges();
+
+            var dto = new ContactTypeCreateDto { Name = "DuplicateName" };
+
+            Func<Task> action = async () => await _repository.CreateContactTypeAsync(dto);
+            action.Should().ThrowAsync<ArgumentException>().WithMessage("Contact Type 'DuplicateName' already exist.");
+        }
+
+        //UpdateContactTypeAsync
+        [Test]
+        public async Task UpdateContactTypeAsync_UpdatesExistingContactType_WhenDataIsValid()
+        {
+            var existingContactType = new ContactType { Id = Guid.NewGuid(), Name = "OriginalName" };
+            _context.ContactType.Add(existingContactType);
+            await _context.SaveChangesAsync();
+
+            var updateDto = new ContactTypeEditDto { Name = "UpdatedName" };
+            await _repository.UpdateContactTypeAsync(existingContactType.Id, updateDto);
+
+            var updatedContactType = await _context.ContactType.FindAsync(existingContactType.Id);
+            updatedContactType.Name.Should().Be("UpdatedName");
+        }
+        [Test]
+        public void UpdateContactTypeAsync_T
+        //UpdateContactTypeStatusAsync
     }
 }
