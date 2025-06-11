2c306335310e6133a101ccbf5b1492709ef57279
yoyocaleb <yoyocaleb1011@gmail.com>
2025-06-11 11:07:48 -0400

    implemented some of the method signatures

--- FMS.Infrastructure/Repositories/ContactRepository.cs
+++ FMS.Infrastructure/Repositories/ContactRepository.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities;
+using FMS.Domain.Repositories;
+using FMS.Domain.Utils;
+using FMS.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace FMS.Infrastructure.Repositories
 {
-    internal class ContactRepository
+    internal class ContactRepository : IContactRepository
     {
+        private readonly FmsDbContext _context;
+
+        public ContactRepository(FmsDbContext context) => _context = context;
+
+        public Task<bool> ContactExistAsync(Guid id) =>
+            _context.Contacts.AnyAsync(e => e.Id == id);
+
+        public async Task<ContactEditDto> GetContactByIdAsync(Guid id)
+        {
+            var contact = await _context.Contacts.AsNoTracking()
+                .SingleOrDefault(e => e.Id == id);
+
+            return contact == null ? null : new ContactEditDto(contact);
+        }
+        public async Task<Chemical> GetContactsByFacilityIdAsync(Guid facilityId)
+        {
+
+        }
+        
+        public async Task<Contact> GetContactsByFacilityIdAndStatusAsync(Guid facilityId, string status)
+        {
+
+        }
+        public async Task<Contact> GetContactsByFacilityIdAndTypeAsync(Guid facilityId, Guid contactTypeId)
+        {
+
+        }
+        public Task AddContactAsync(ContactCreateDto contact)
+        {
+
+        }
     }
+
 }
