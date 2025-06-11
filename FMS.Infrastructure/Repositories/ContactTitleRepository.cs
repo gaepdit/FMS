47888a2317fff82cf11d4c8b82626d9e8cbb17b8
yoyocaleb <yoyocaleb1011@gmail.com>
2025-06-11 11:25:32 -0400

    Initial commit

---
+++ FMS.Infrastructure/Repositories/ContactTitleRepository.cs
@@ -0,0 +1,18 @@
+﻿using FMS.Domain.Dto;
+using FMS.Domain.Entities;
+using FMS.Domain.Repositories;
+using FMS.Domain.Utils;
+using FMS.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FMS.Infrastructure.Repositories
+{
+    public class ContactTitleRepository : IContactTitleRepository
+    {
+        private readonly FmsDbContext _context;
+    }
+}
