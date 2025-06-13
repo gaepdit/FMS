2d57029da5faade1143f09c46cc183cf48af47be
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-06-13 11:48:05 -0400

    Implements Groundwater Status repository
    Implements the Groundwater Status repository with basic CRUD operations.
    
    This includes methods for retrieving, listing, and checking the existence of Groundwater Status entities.
    Also injects the FmsDbContext to the GroundwaterStatusRepository class.

--- FMS.Infrastructure/Repositories/GroundwaterStatusRepository.cs
+++ FMS.Infrastructure/Repositories/GroundwaterStatusRepository.cs
@@ -1,5 +1,7 @@
 ﻿using FMS.Domain.Dto;
 using FMS.Domain.Repositories;
+using FMS.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,37 +12,49 @@ namespace FMS.Infrastructure.Repositories
 {
     public class GroundwaterStatusRepository : IGroundwaterStatusRepository
     {
+        public readonly FmsDbContext _context;
+        public GroundwaterStatusRepository(FmsDbContext context) => _context = context;
+
+        // Implement interface methods
+        public Task<bool> GroundwaterStatusExistsAsync(Guid id) =>
+            _context.GroundwaterStatuses.AnyAsync(e => e.Id == id);
+
+        public Task<bool> GroundwaterStatusNameExistsAsync(string name, Guid? ignoreId = null) => _context.BudgetCodes.AnyAsync(e => e.Name == name && (!ignoreId.HasValue || e.Id != ignoreId.Value));
+
+        public async Task<GroundwaterStatusEditDto> GetGroundwaterStatusAsync(Guid id)
+        {
+            var groundwaterStatus = await _context.GroundwaterStatuses.AsNoTracking()
+                .SingleOrDefaultAsync(e => e.Id == id);
+
+            if (groundwaterStatus == null)
+            {
+                return null;
+            }
+
+            return new GroundwaterStatusEditDto(groundwaterStatus);
+        }
+
+        public async Task<IReadOnlyList<GroundwaterStatusSummaryDto>> GetGroundwaterStatusListAsync() => await _context.GroundwaterStatuses.AsNoTracking()
+           .OrderBy(e => e.Name)
+           .Select(e => new GroundwaterStatusSummaryDto(e))
+           .ToListAsync();
+
+        public async Task<string> GetGroundwaterStatusNameAsync(Guid? id)
+        {
+            var groundwaterStatus = await _context.GroundwaterStatuses.AsNoTracking()
+                .SingleOrDefaultAsync(e => e.Id == id);
+
+            return groundwaterStatus?.Name;
+        }
+
         public Task<Guid> CreateGroundwaterStatusAsync(GroundwaterStatusCreateDto groundwaterStatus)
         {
             throw new NotImplementedException();
         }
 
-        public Task<GroundwaterStatusEditDto> GetGroundwaterStatusAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IReadOnlyList<GroundwaterStatusSummaryDto>> GetGroundwaterStatusListAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<string> GetGroundwaterStatusNameAsync(Guid? id)
-        {
-            throw new NotImplementedException();
-        }
+        
 
         public Task<bool> GroundwaterStatusDescriptionExistsAsync(string description, Guid? ignoreId = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> GroundwaterStatusExistsAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> GroundwaterStatusNameExistsAsync(string name, Guid? ignoreId = null)
         {
             throw new NotImplementedException();
         }
