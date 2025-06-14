1f3f9f69bbe4ed90ba5dd8730eafcfa8ab54ba90
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-06-14 17:24:42 -0400

    OverallStatus repository
    Initial commit to OverallStatusRepository.
    
    This change reflects a broader scope of the repository, which now handles overall statuses instead of just groundwater statuses. The class and interface names have been updated accordingly. The implementation has been replaced with placeholder NotImplementedExceptions, indicating that the new repository requires further implementation work.

---
+++ FMS.Infrastructure/Repositories/OverallStatusRepository.cs
@@ -0,0 +1,79 @@
+﻿using FMS.Domain.Dto;
+using FMS.Domain.Entities;
+using FMS.Domain.Repositories;
+using FMS.Domain.Utils;
+using FMS.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FMS.Infrastructure.Repositories
+{
+    
+    public class OverallStatusRepository : IOverallStatusRepository
+    {
+        public Task<bool> OverallStatusExistsAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<OverallStatusEditDto> GetOverallStatusByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Guid> CreateOverallStatusAsync(OverallStatusCreateDto overallStatus)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<OverallStatusSummaryDto>> GetOverallStatusListsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOverallStatusAsync(Guid Id, OverallStatusEditDto overallStatusUpdates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateOverallStatusStatusAsync(Guid id, bool active)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region IDisposable Implementation
+
+        private bool _disposed = false;
+
+        ~OverallStatusRepository()
+        {
+            Dispose(false);
+        }
+
+        // Implement IDisposable pattern
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    // Dispose managed resources here if necessary
+                }
+
+                // Dispose unmanaged resources here if necessary
+                _disposed = true;
+            }
+        }
+
+        #endregion
+    }
+}
