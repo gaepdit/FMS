fa4f23fcf6abbe5728b13b3a16fcb045569a532d
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-06-16 19:00:44 -0400

    Adds Parcel Type repository and DTO
    Adds the Parcel Type repository and associated DTOs,
    allowing for data access and manipulation of parcel type
    entities. The repository includes methods for creating,
    reading, updating, and deactivating parcel types.

--- FMS.Domain/Dto/ParcelType/ParcelTypeSummaryDto.cs
+++ FMS.Domain/Dto/ParcelType/ParcelTypeSummaryDto.cs
@@ -1,7 +1,20 @@
-﻿namespace FMS.Domain.Dto
+﻿using FMS.Domain.Entities;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FMS.Domain.Dto
 {
     public class ParcelTypeSummaryDto
     {
+        public ParcelTypeSummaryDto(ParcelType parcelType)
+        {
+            Name = parcelType.Name;
+        }
+
+        public Guid Id { get; }
+
+        public bool Active { get; }
+
         public string Name { get; set; }
     }
 }
