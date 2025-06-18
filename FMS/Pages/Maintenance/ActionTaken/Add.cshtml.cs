c6e917a4f8def05dfec40b16d75dce64a41ae991
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-06-17 21:29:29 -0400

    Adds Action Taken add page
    Adds the initial page and model for creating new Action Taken records in the maintenance section.

---
+++ FMS/Pages/Maintenance/ActionTaken/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMS.Pages.Maintenance.ActionTaken
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
