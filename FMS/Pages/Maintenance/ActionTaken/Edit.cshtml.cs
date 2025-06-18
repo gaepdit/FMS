258c158e002a18b17a2487e86e00b361f2dd6386
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-06-17 21:27:37 -0400

    Adds ActionTaken Edit page
    Adds the basic Edit page and page model for the ActionTaken feature.
    This provides the initial structure for editing existing ActionTaken records.

---
+++ FMS/Pages/Maintenance/ActionTaken/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMS.Pages.Maintenance.ActionTaken
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
