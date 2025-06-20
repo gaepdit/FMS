8f660d655d1882b5d2d03b1b9aadb0c5606400be
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-06-20 18:54:39 -0400

    Removes unnecessary using statements.
    Removes redundant and unused namespace imports to improve code clarity and maintainability.

--- FMS.Infrastructure/Repositories/ContactRepository.cs
+++ FMS.Infrastructure/Repositories/ContactRepository.cs
@@ -3,16 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FMS.Domain.Dto;
-using FMS.Domain.Dto;
 using FMS.Domain.Entities;
 using FMS.Domain.Repositories;
 using FMS.Domain.Utils;
 using FMS.Infrastructure.Contexts;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace FMS.Infrastructure.Repositories
 {
