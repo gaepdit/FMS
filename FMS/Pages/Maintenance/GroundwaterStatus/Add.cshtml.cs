f937afebf72b677ccd81e3374384252b5aa3ea8b
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-07-01 19:38:47 -0400

    Adds Groundwater Status maintenance pages
    Adds the initial structure for the Groundwater Status maintenance pages, including Index, Add, and Edit functionalities.
    
    This provides a basic framework for managing Groundwater Status data within the application.

---
+++ FMS/Pages/Maintenance/GroundwaterStatus/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMS.Pages.Maintenance.GroundwaterStatus
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FMS/Pages/Maintenance/GroundwaterStatus/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMS.Pages.Maintenance.GroundwaterStatus
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
