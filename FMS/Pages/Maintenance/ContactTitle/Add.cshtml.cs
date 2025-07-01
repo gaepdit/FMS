8c626ce8a7a00e5f63140e4932c988e2f501971f
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-07-01 11:08:40 -0400

    Adds functionality to create contact titles
    Enables users with the SiteMaintenance role to create new contact titles.
    
    This commit adds a new page and associated logic for creating contact titles,
    including server-side validation to prevent duplicate names. It also adds
    a repository method to check if a contact title name already exists.

--- FMS/Pages/Maintenance/ContactTitle/Add.cshtml.cs
+++ FMS/Pages/Maintenance/ContactTitle/Add.cshtml.cs
@@ -1,12 +1,54 @@
+using System.Threading.Tasks;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Users;
+using FMS.Domain.Repositories;
+using FMS.Platform.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FMS.Pages.Maintenance.ContactTitle
 {
+    [Authorize(Roles = UserRoles.SiteMaintenance)]
     public class AddModel : PageModel
     {
+        private readonly IContactTitleRepository _repository;
+        public AddModel(IContactTitleRepository repository) => _repository = repository;
+
+        [BindProperty]
+        public ContactTitleCreateDto ContactTitle { get; set; }
+
         public void OnGet()
         {
+            // Method intentionally left empty.
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            ContactTitle.TrimAll();
+
+            if (await _repository.ContactTitleNameExistsAsync(ContactTitle.Name))
+            {
+                ModelState.AddModelError("ContactType.Name", "Name entered already exists.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _repository.CreateContactTitleAsync(ContactTitle);
+
+            TempData?.SetDisplayMessage(Context.Success,
+                $"{MaintenanceOptions.ContactTitle} \"{ContactTitle.Name}\" successfully created.");
+
+            return RedirectToPage("./Index", "select",
+                new { MaintenanceSelection = MaintenanceOptions.ContactTitle });
         }
     }
 }
