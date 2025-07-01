3ed9a123ac6f6c187302b067f3bf0361406aa025
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-07-01 16:02:42 -0400

    Adds Funding Source maintenance pages
    Adds the basic pages and model classes for the Funding Source maintenance feature, including Index, Add, and Edit pages.
    
    Also adds a link to the Funding Source Index page to the main Maintenance Index page.
    
    Registers the FundingSourceRepository in Startup.cs.

---
+++ FMS/Pages/Maintenance/FundingSource/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMS.Pages.Maintenance.FundingSource
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
