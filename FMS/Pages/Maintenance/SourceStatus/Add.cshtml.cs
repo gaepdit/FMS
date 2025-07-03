084b1fbf104bbb51fc232592f45df59024e7f17e
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-07-03 13:41:33 -0400

    Adds Source Status maintenance pages
    This commit introduces the ability to manage Source Statuses within the system.
    
    Adds Index, Add, and Edit pages, including associated models and repositories, to manage the Source Status entities.
    This enhancement allows authorized users to create, edit, and remove Source Status entries.

--- FMS/Pages/Maintenance/SourceStatus/Add.cshtml.cs
+++ FMS/Pages/Maintenance/SourceStatus/Add.cshtml.cs
@@ -1,12 +1,59 @@
+using System.Threading.Tasks;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Users;
+using FMS.Domain.Repositories;
+using FMS.Platform.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FMS.Pages.Maintenance.SourceStatus
 {
+    [Authorize(Roles = UserRoles.SiteMaintenance)]
     public class AddModel : PageModel
     {
+        private readonly ISourceStatusRepository _repository;
+        public AddModel(ISourceStatusRepository repository) => _repository = repository;
+
+        [BindProperty]
+        public SourceStatusCreateDto SourceStatus { get; set; }
+
         public void OnGet()
         {
+            // Method intentionally left empty.
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            SourceStatus.TrimAll();
+
+            if (await _repository.SourceStatusNameExistsAsync(SourceStatus.Name))
+            {
+                ModelState.AddModelError("SourceStatus.Name", "Name entered already exists.");
+            }
+
+            if (await _repository.SourceStatusNameExistsAsync(SourceStatus.Description))
+            {
+                ModelState.AddModelError("SourceStatus.Description", "Description entered already exists.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            await _repository.CreateSourceStatusAsync(SourceStatus);
+
+            TempData?.SetDisplayMessage(Context.Success,
+                $"{MaintenanceOptions.SourceStatus} \"{SourceStatus.Name}\" successfully created.");
+
+            return RedirectToPage("./Index",
+                new { MaintenanceSelection = MaintenanceOptions.SourceStatus });
         }
     }
 }
--- FMS/Pages/Maintenance/SourceStatus/Edit.cshtml.cs
+++ FMS/Pages/Maintenance/SourceStatus/Edit.cshtml.cs
@@ -1,12 +1,87 @@
+using System;
+using System.Threading.Tasks;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Users;
+using FMS.Domain.Repositories;
+using FMS.Platform.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace FMS.Pages.Maintenance.SourceStatus
 {
+    [Authorize(Roles = UserRoles.SiteMaintenance)]
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly ISourceStatusRepository _repository;
+        public EditModel(ISourceStatusRepository repository) => _repository = repository;
+
+        [BindProperty]
+        public SourceStatusEditDto SourceStatus { get; set; }
+
+        [BindProperty]
+        public Guid Id { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Id = id.Value;
+            SourceStatus = await _repository.GetSourceStatusAsync(id.Value);
+
+            if (SourceStatus == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            SourceStatus.TrimAll();
+
+            // If editing Code, make sure the new Code doesn't already exist before trying to save.
+            if (await _repository.SourceStatusNameExistsAsync(SourceStatus.Name, Id))
+            {
+                ModelState.AddModelError("SourceStatus.Name", "Name entered already exists.");
+            }
+
+            if (await _repository.SourceStatusDescriptionExistsAsync(SourceStatus.Description, Id))
+            {
+                ModelState.AddModelError("SourceStatus.Description", "Description entered already exists.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+                await _repository.UpdateSourceStatusAsync(Id, SourceStatus);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await _repository.SourceStatusExistsAsync(Id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            TempData?.SetDisplayMessage(Context.Success, $"{MaintenanceOptions.SourceStatus} \"{SourceStatus.Name}\" successfully updated.");
+            return RedirectToPage("./Index");
         }
     }
 }
