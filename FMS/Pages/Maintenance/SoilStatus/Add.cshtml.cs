94295b7da550452e42829c7be244e6a6cfe5c5e5
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-07-02 22:01:57 -0400

    Adds Soil Status maintenance pages
    Adds the initial Index, Add, and Edit pages for maintaining Soil Status entities.
    These pages provide a basic structure for CRUD operations related to soil status.

---
+++ FMS/Pages/Maintenance/SoilStatus/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMS.Pages.Maintenance.SoilStatus
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FMS/Pages/Maintenance/SoilStatus/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMS.Pages.Maintenance.SoilStatus
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
