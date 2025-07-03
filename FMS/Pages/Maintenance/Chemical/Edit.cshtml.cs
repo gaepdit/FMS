e05e9d74f794c0e86d7056dd803ad6f0da9f187f
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-07-03 16:35:28 -0400

    Adds Chemical edit functionality
    Implements the edit page for the Chemical entity in the maintenance section.
    
    This includes:
    - Creating the Razor Page and corresponding PageModel.
    - Implementing validation to prevent duplicate records (CasNo, ChemicalName, CommonName).
    - Using EditDto object for data transfer.
    - Displaying success message upon updating the Chemical.
    
    Also adjusts the alert timeout.

--- FMS/Pages/Maintenance/Chemical/Edit.cshtml.cs
+++ FMS/Pages/Maintenance/Chemical/Edit.cshtml.cs
@@ -1,12 +1,92 @@
+using System;
+using System.Threading.Tasks;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Users;
+using FMS.Domain.Repositories;
+using FMS.Platform.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
-namespace FMS.Pages.Maintenance.Chemicals
+namespace FMS.Pages.Maintenance.Chemical
 {
+    [Authorize(Roles = UserRoles.SiteMaintenance)]
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly IChemicalRepository _repository;
+        public EditModel(IChemicalRepository repository) => _repository = repository;
+
+        [BindProperty]
+        public ChemicalEditDto Chemical { get; set; }
+
+        [BindProperty]
+        public Guid Id { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(Guid? id)
         {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Id = id.Value;
+            Chemical = await _repository.GetChemicalByIdAsync(id.Value);
+
+            if (Chemical == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Chemical.TrimAll();
+
+            // If editing Chemical, make sure the new Chemical doesn't already exist before trying to save.
+            if (await _repository.ChemicalCasNoExistsAsync(Chemical.CasNo, Id))
+            {
+                ModelState.AddModelError("Chemical.CasNo", "CasNo entered already exists.");
+            }
+
+            if (await _repository.ChemicalChemicalNameExistsAsync(Chemical.ChemicalName, Id))
+            {
+                ModelState.AddModelError("Chemical.ChemicalName", "Chemical Name entered already exists.");
+            }
+
+            if (await _repository.ChemicalCommonNameExistsAsync(Chemical.CommonName, Id))
+            {
+                ModelState.AddModelError("Chemical.CommonName", "Common Name entered already exists.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            try
+            {
+                await _repository.UpdateChemicalAsync(Id, Chemical);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!await _repository.ChemicalExistsAsync(Id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            TempData?.SetDisplayMessage(Context.Success, $"{MaintenanceOptions.Chemical} \"{Chemical.ChemicalName}\" successfully updated.");
+            return RedirectToPage("./Index");
         }
     }
 }
--- FMS/wwwroot/js/formIndex.js
+++ FMS/wwwroot/js/formIndex.js
@@ -1,3 +1,3 @@
 ﻿$(document).ready(function formIndex() {
-    setTimeout(function () { $('.alert').alert('close') }, 10000)
+    setTimeout(function () { $('.alert').alert('close') }, 6000)
 });
