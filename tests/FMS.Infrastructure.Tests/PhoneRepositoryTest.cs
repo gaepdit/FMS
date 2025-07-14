b31b9ac061cf027b4ba612568df5365d6768b531
yoyocaleb <yoyocaleb1011@gmail.com>
2025-07-14 12:12:31 -0400

    Added Setup and First Test for PhoneRepositories

--- tests/FMS.Infrastructure.Tests/PhoneRepositoryTest.cs
+++ tests/FMS.Infrastructure.Tests/PhoneRepositoryTest.cs
@@ -1,12 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NSubstitute;
+using System;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using FMS.Infrastructure.Contexts;
+using FMS.Infrastructure.Repositories;
+using FMS.Domain.Entities;
+using FMS.Domain.Dto;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
 
 namespace FMS.Infrastructure.Tests
 {
-    internal class PhoneRepositoryTest
+    [TestFixture]
+    public class PhoneRepositoryTest
     {
+        private FmsDbContext _context;
+        private PhoneRepositoryTest _repository;
+        private bool _disposed = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<FmsDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{Guid.NewGuid()}")
+                .Options;
+
+            var httpContextAccessor = Substitute.For<IHttpContextAccessor>();
+            _context = new FmsDbContext(options, httpContextAccessor);
+            _repository = new PhoneRepository(_context);
+
+            _context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _context.Database.EnsureDeleted();
+                _context.Dispose();
+                _repository.Dispose();
+                _disposed = true;
+            }
+        }
+        
+        [Test]
+        public async Task PhoneNumberExistsAsync_ReturnCorrectPhoneNumber_WhenIdIsValid()
+        {
+            var PhoneNumber = new Phone
+            {
+                Id = Guid.NewGuid(),
+                Number = "VALID_NUMBER",
+                Active = true
+            };
+            _context.Phones.Add(PhoneNumber);
+            await _context.SaveChangesAsync();
+
+            var result = await _repository.PhoneNumberExistsAsync(PhoneNumber.Id);
+        }
     }
 }
