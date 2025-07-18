169ab5f30df49c97da51aa4817a4d5325db6fbf3
yoyocaleb <yoyocaleb1011@gmail.com>
2025-07-18 11:07:52 -0400

    GetPhoneByIdAsync Test added

--- tests/FMS.Infrastructure.Tests/PhoneRepositoryTest.cs
+++ tests/FMS.Infrastructure.Tests/PhoneRepositoryTest.cs
@@ -95,5 +95,24 @@ namespace FMS.Infrastructure.Tests
             result.Should().BeFalse();
         }
 
+        [Test]
+        public async Task GetPhoneByIdAsync_WhenIdExist()
+        {
+            var existingPhone = await _context.Phones.FirstAsync();
+            var result = await _repository.GetPhoneByIdAsync(existingPhone.Id);
+
+            result.Should().NotBeNull();
+            result.Should().BeOfType<PhoneEditDto>();
+            result.Id.Should().Be(existingPhone.Id);
+            result.Number.Should().Be(existingPhone.Number);
+        }
+        [Test]
+        public async Task GetPhoneByIdAsync_WhenIdDoesNotExist_ReturnsNull()
+        {
+            var nonExistingId = Guid.NewGuid();
+            var result = await _repository.GetPhoneByIdAsync(nonExistingId);
+
+            result.Should().BeNull();
+        }
     }
 }
