a02804a445c5e38d84cb06511facc27b9fa364e7
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-08-13 01:23:22 -0400

    Refactors GapsAssessment repository and adds edit page
    Refactors the GapsAssessment repository to use DTOs for data transfer and adds a name uniqueness check. This improves data handling and prevents duplicate Gaps Assessment names. Also, the GapsAssessment is retrieved as a DTO from the database.
    
    Adds scaffolding for the Edit and Add pages related to the GapsAssessment.

---
+++ FMS/Pages/Maintenance/GapsAssessment/Add.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMS.Pages.Maintenance.GapsAssessment
+{
+    public class AddModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
