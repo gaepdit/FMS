b2574723efe658cea4399abe208fb75c7fcb2446
yoyocaleb <yoyocaleb1011@gmail.com>
2025-08-13 11:29:46 -0400

    Created ContactTitleRepositoryTest. Initial Commit

---
+++ tests/FMS.Infrastructure.Tests/ContactTitleRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NSubstitute;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FMS.Infrastructure.Contexts;
+using FMS.Infrastructure.Repositories;
+using FMS.Domain.Entities;
+using FMS.Domain.Dto;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+using System.Collections.Generic;
+
+namespace FMS.Infrastructure.Tests
+{
+    [TestFixture]
+    public class ContactTitleRepositoryTest
+    {
+        private FmsDbContext _context;
+        private ContactTitleRepository _repository;
+        private bool _disposed = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<FmsDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{Guid.NewGuid()}")
+                .Options;
+        }
+    }
+}
