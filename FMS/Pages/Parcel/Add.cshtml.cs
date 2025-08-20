ae23c49dd27e88255bb5c3a6500adc5a630ffbca
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-08-20 00:08:42 -0400

    Adds Parcel Add page
    Adds the Parcel Add page with basic structure.
    Includes authorization for File Creator or Editor roles.

--- FMS/Pages/Parcel/Add.cshtml.cs
+++ FMS/Pages/Parcel/Add.cshtml.cs
@@ -1,8 +1,21 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FMS.Domain.Data;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Users;
+using FMS.Domain.Repositories;
+using FMS.Helpers;
+using FMS.Platform.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.IdentityModel.Tokens;
 
 namespace FMS.Pages.Parcel
 {
+    [Authorize(Policy = UserPolicies.FileCreatorOrEditor)]
     public class AddModel : PageModel
     {
         public void OnGet()
