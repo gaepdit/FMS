4e452bd3321bebdb83987bd78a27e29c8b03e8c9
yoyocaleb <yoyocaleb1011@gmail.com>
2025-08-28 12:13:49 -0400

    Created Contact Test

---
+++ tests/FMS.Infrastructure.Tests/ContactRepositoryTest.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NSubstitute;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FMS.Infrastructure.Contexts;
+using FMS.Infrastructure.Repositories;
+using FMS.Domain.Entities;
+using FMS.Domain.Dto;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+using System.Collections.Generic;
+
+namespace FMS.Infrastructure.Tests
+{
+    [TestFixture]
+    public class ContactRepositoryTest
+    {
+        private FmsDbContext _context;
+        private ContactRepository _repository;
+        private bool _disposed = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<FmsDbContext>()
+            .UseInMemoryDatabase(databaseName: $"TestDatabase_{Guid.NewGuid()}")
+            .Options;
+            var httpContextAccessor = Substitute.For<HttpContextAccessor>();
+            _context = new FmsDbContext(options, httpContextAccessor);
+            _repository = new ContactRepository(_context);
+
+            _context.Contacts.Add(new Contact
+            {
+                Id = Guid.NewGuid(),
+
+            });
+            _context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _context.Database.EnsureCreated();
+                _context.Dispose();
+                _repository.Dispose();
+                _disposed = true;
+            }
+        }
+
+        //Test
+    }
+}
