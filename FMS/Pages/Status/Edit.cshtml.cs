77f8cc0e17d9fe3ae45dd6a7e4cc70ed95e6edbf
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-09-02 20:34:34 -0400

    Passes facility ID to Status Edit page
    Ensures the Facility ID is passed to the Status Edit page,
    allowing users to edit the status details for the
    correct facility.
    Removes obsolete Status Add page and associated code.

--- FMS/Pages/Status/Edit.cshtml.cs
+++ FMS/Pages/Status/Edit.cshtml.cs
@@ -1,8 +1,17 @@
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Users;
+using FMS.Domain.Entities;
+using FMS.Domain.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Threading.Tasks;
 
 namespace FMS.Pages.Status
 {
+    [Authorize(Roles = UserRoles.FileEditor)]
     public class EditModel : PageModel
     {
         public void OnGet()
