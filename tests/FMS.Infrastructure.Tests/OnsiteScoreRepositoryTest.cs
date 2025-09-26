5ee5d6ddc6162649cac4b20e63f6df9c78e3a94d
yoyocaleb <yoyocaleb1011@gmail.com>
2025-09-26 11:23:04 -0400

    Test file made

---
+++ tests/FMS.Infrastructure.Tests/OnsiteScoreRepositoryTest.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities;
+using FMS.Infrastructure.Contexts;
+using FMS.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FMS.Infrastructure.Tests
+{
+    [TestFixture]
+    public class OnsiteScoreRepositoryTest
+    {
+        private FmsDbContext _context;
+        private ContactRepository _repository;
+        private bool _disposed;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<FmsDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{Guid.NewGuid()}")
+                .Options;
+            var httpContextAccessor = Substitute.For<HttpContextAccessor>();
+            _context = new FmsDbContext(options, httpContextAccessor);
+
+            _context.OnsiteScores.Add(new OnsiteScore
+            {
+                Id = Guid.Empty,
+                
+            });
+            _context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Dispose();
+        }
+
+        private void Dispose()
+        {
+            if (!_disposed)
+            {
+                _context.Database.EnsureCreated();
+                _context.Dispose();
+                _repository.Dispose();
+                _disposed = true;
+            }
+        }
+
+       
+    }
+}
