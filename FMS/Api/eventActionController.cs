533efdd4e2ed9c1079092f10ad534e11335525a8
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-10-15 09:04:35 -0400

    Adds Event Action API Controller
    Adds an API controller for handling event actions.
    
    Introduces client-side JavaScript for populating staff dropdowns based on office selection,
    allowing filtering for assignment purposes.

---
+++ FMS/Api/eventActionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FMS.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class eventActionController : ControllerBase
+    {
+        //[HttpGet("{id:guid}/staff")]
+        //public async Task<JsonResult> GetStaffAsync([FromRoute] Guid id) =>
+        //new(await officeService.GetStaffAsListItemsAsync(id));
+
+        //[HttpGet("{id:guid}/all-staff")]
+        //public async Task<IActionResult> GetAllStaffAsync([FromRoute] Guid id) =>
+        //    await authorization.Succeeded(User, Policies.ActiveUser)
+        //        ? new JsonResult(await officeService.GetStaffAsListItemsAsync(id, includeInactive: true))
+        //        : Unauthorized();
+
+        //[HttpGet("{id:guid}/staff-for-assignment")]
+        //public async Task<IActionResult> GetStaffForAssignmentAsync([FromRoute] Guid id)
+        //{
+        //    if (!await authorization.Succeeded(User, Policies.ActiveUser)) return Unauthorized();
+
+        //    var office = await officeService.FindAsync(id);
+
+        //    return new JsonResult(await authorization.Succeeded(User, office, new OfficeAssignmentRequirement())
+        //        ? await officeService.GetStaffAsListItemsAsync(id)
+        //        : null);
+        //}
+    }
+}
---
+++ FMS/wwwroot/js/actionSelect.js
@@ -0,0 +1,42 @@
+﻿// Populate the associate dropdown when an office selection is made.
+function setUpStaffDropdown(officeElementId, staffElementId, forAssignment, placeholder) {
+    let apiPath;
+
+    if (forAssignment === true) {
+        apiPath = "staff-for-assignment";
+    } else {
+        apiPath = "all-staff";
+    }
+
+    const officeSelect = document.getElementById(officeElementId);
+
+    officeSelect.addEventListener("change", () => {
+        const staffSelect = document.getElementById(staffElementId);
+        staffSelect.innerHTML = `<option value="">${placeholder}</option>`;
+        staffSelect.disabled = true;
+        if (officeSelect.value === '') return;
+
+        axios.get(`/api/offices/${officeSelect.value}/${apiPath}`)
+            .then(function (response) {
+                const data = response.data;
+                if (data == null || data.length === 0) return;
+
+                const onlyUnassignedCheckbox = document.getElementById("Spec_OnlyUnassigned");
+                staffSelect.disabled = onlyUnassignedCheckbox ? onlyUnassignedCheckbox.checked : false;
+
+                let opt;
+                for (const item of data) {
+                    opt = document.createElement('option');
+                    opt.text = item.name;
+                    opt.value = item.id;
+                    staffSelect.add(opt);
+                }
+            })
+            .catch(function errorHandler(error) {
+                staffSelect.innerHTML = '<option value="">Error</option>';
+                if (error instanceof Error && typeof rg4js === "function") {
+                    rg4js('send', { error: error, tags: ['handled_promise_rejection'] });
+                }
+            });
+    });
+}
