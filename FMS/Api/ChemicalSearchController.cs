e6a8bf0cc3eae3387c5b7f1a543f06a2f1d6a9ce
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-10-22 22:34:33 -0400

    Adds API endpoint for chemical search
    Introduces an API endpoint that allows users to search for chemicals by name.
    
    This endpoint enhances the application's functionality by enabling
    dynamic searching and auto-completion for chemical names,
    improving the user experience in forms and data entry.
    Related to #815

--- FMS/Api/ChemicalSearchController.cs
+++ FMS/Api/ChemicalSearchController.cs
@@ -1,11 +1,46 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DocumentFormat.OpenXml.Office2010.Excel;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Users;
+using FMS.Domain.Repositories;
+using FMS.Domain.Services;
+using FMS.Infrastructure;
+using FMS.Infrastructure.Repositories;
+using FMS.Infrastructure.Contexts;
+using FMS.Platform.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using TestSupport.SeedDatabase;
+using System.Linq;
+
 
 namespace FMS.Api
 {
-    [Route("api/[controller]")]
+    [Authorize]
     [ApiController]
-    public class ChemicalSearchController : ControllerBase
+    [Route("api/chemsearch")]
+    public class ChemicalSearchController(FmsDbContext _context) : ControllerBase
     {
+        [HttpGet("{query:string}")]
+        public async Task<IActionResult> Get([FromRoute] string query)
+        {
+            if (string.IsNullOrWhiteSpace(query))
+            {
+                return Ok(new List<string>()); // Return empty if no query
+            }
+
+            var results = await _context.Chemicals.AsNoTracking()
+                                        .Where(e => e.ChemicalName.ToString().Contains(query)) // Example filtering
+                                        .Select(e => e.ChemicalName) // Select relevant data
+                                        .ToListAsync();
+
+            return Ok(results);
+        }
     }
 }
