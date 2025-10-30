ef35162ff1276b7e48d3d5347e111bac3e4a4881
Tom Karasch <tom.karasch@dnr.ga.gov>
2025-10-30 15:50:02 -0400

    Displays Funding Source Description
    Updates the facility details page to display the funding source description instead of the name.
    
    Adds descriptions to the FundingSource seed data for clarity in tests.
    
    Improves user experience by providing more context for funding sources.
    
    Related to #827

--- tests/TestHelpers/SeedData/FundingSourceSeedData.cs
+++ tests/TestHelpers/SeedData/FundingSourceSeedData.cs
@@ -17,36 +17,42 @@ namespace FMS.TestData.SeedData
                     Id = new Guid("97E9B7AF-5A60-4426-812A-8F6B5B2CB635"),
                     Active = true,
                     Name = "A",
+                    Description = "Abandoned",
                 },
                 new()
                 {
                     Id = new Guid("DEACC0E4-AB9F-4A16-8B66-FB7512731AB5"),
                     Active = true,
                     Name = "LE",
+                    Description = "Local Government Eligible",
                 },
                 new()
                 {
                     Id = new Guid("65A97123-4BCA-478C-B110-E0F149DFCCD1"),
                     Active = true,
                     Name = "LI",
+                    Description = "Local Government Ineligible",
                 },
                 new()
                 {
                     Id = new Guid("5DD80C99-665C-475C-8799-203915D6E668"),
                     Active = true,
                     Name = "P",
+                    Description = "Private",
                 },
                 new()
                 {
                     Id = new Guid("5F857A62-65B0-475D-B31C-A1F685C5C002"),
                     Active = true,
                     Name = "SE",
+                    Description = "State Government Eligible",
                 },
                 new()
                 {
                     Id = new Guid("CEDC5C85-E355-4458-A283-323D8F7D075A"),
                     Active = true,
                     Name = "SI",
+                    Description = "State Government Ineligible",
                 }
             };
         }
