32cbbc844aae6e99c7cf181576d2884c2e201cfe
yoyocaleb <yoyocaleb1011@gmail.com>
2025-11-04 11:35:30 -0500

    Initial Test file created

---
+++ tests/FMS.Infrastructure.Tests/EventRepositoryTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FMS.Domain.Entities;
+using FMS.Infrastructure.Contexts;
+using FMS.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace FMS.Infrastructure.Tests
+{
+    [TestFixture]
+    public class EventRepositoryTest
+    {
+        private FmsDbContext _context;
+        private EventRepository _repository;
+        private bool _disposed = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<FmsDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{Guid.NewGuid()}")
+                .Options;
+            var httpContextAccessor = Substitute.For<HttpContextAccessor>();
+            _context = new FmsDbContext(options, httpContextAccessor);
+            _repository = new EventRepository(_context);
+
+            _context.Events.Add (new Event
+            {
+                Id = Guid.NewGuid(),
+                
+            });
+            _context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _context.Database.EnsureCreated();
+                _context.Dispose();
+                _repository.Dispose();
+                _disposed = true;
+            }
+        }
+    }
+}
