35086cf6da1c69f06ce046eaaac3c3c2a99b8900
yoyocaleb <yoyocaleb1011@gmail.com>
2025-12-09 10:19:43 -0500

    Most test created, not completed

--- tests/FMS.Infrastructure.Tests/AllowedActionTakenRepositoryTest.cs
+++ tests/FMS.Infrastructure.Tests/AllowedActionTakenRepositoryTest.cs
@@ -3,10 +3,146 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FluentAssertions;
+using FMS.Domain.Dto;
+using FMS.Domain.Entities;
+using FMS.Infrastructure.Contexts;
+using FMS.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
+using NUnit.Framework;
+using Xunit.Extensions.AssertExtensions;
 
 namespace FMS.Infrastructure.Tests
 {
-    internal class AllowedActionTakenRepositoryTest
+    [TestFixture]
+    public class AllowedActionTakenRepositoryTest
     {
+        private FmsDbContext _context;
+        private AllowedActionTakenRepository _repository;
+        private bool _disposed = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<FmsDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{Guid.NewGuid()}")
+                .Options;
+            var httpContextAccessor = Substitute.For<HttpContextAccessor>();
+            _context = new FmsDbContext(options, httpContextAccessor);
+            _repository = new AllowedActionTakenRepository(_context);
+
+            _context.AllowedActionsTaken.Add(new AllowedActionTaken
+            {
+                Id = Guid.NewGuid(),
+                Active = true,
+
+            });
+            _context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _context.Database.EnsureCreated();
+                _context.Dispose();
+                _repository.Dispose();
+                _disposed = true;
+            }
+        }
+
+        // AllowedActionTakenExistAsync (id only)
+        [Test]
+        public async Task AllowedActionTakenExistAsync_ReturnsTrue_WhenAllowedActionTakenExist()
+        {
+            var existingAAT = await _context.AllowedActionsTaken.Select(ft => ft.Id).FirstAsync();
+            var results = await _repository.AllowedActionTakenExistsAsync(existingAAT);
+            results.Should().BeTrue();
+        }
+        [Test]
+        public async Task AllowedActionTakenExistAsync_ReturnsFalse_WhenDataIsInvalid()
+        {
+            var nonExistingAAT = Guid.NewGuid();
+            var results = await _repository.AllowedActionTakenExistsAsync(nonExistingAAT);
+            results.Should().BeFalse();
+        }
+
+        // AllowedActionTakenExistAsync (eventTypeId and actionTakenId)
+
+        // GetAllowedActionTakenByAATIdAsync
+        [Test]
+        public async Task GetAllowedActionTakenByAATIdAsync_ReturnsAllowedActionTakenSpec_WhenIdExist()
+        {
+            var existingAAT = await _context.AllowedActionsTaken.FirstAsync();
+            var results = await _repository.GetAllowedActionTakenByAATIdAsync(existingAAT.Id);
+
+            results.Should().NotBeNull();
+            results.Should().BeOfType<AllowedActionTakenSpec>();
+            results.Id.Should().Be(existingAAT.Id);
+            results.Active.Should().Be(existingAAT.Active);
+        }
+        [Test]
+        public async Task GetAllowedActionTakenByAATIdAsync_ReturnsNull_WhenIdDoesNotExist()
+        {
+            var nonexistingAAT = Guid.NewGuid();
+            var results = await _repository.GetAllowedActionTakenByAATIdAsync(nonexistingAAT);
+            results.Should().BeNull();
+        }
+
+        // GetAllowedActionTakenListAsync
+
+        // CreateAllowedActionTakenAsync
+        [Test]
+        public async Task CreateAllowedActionTakenAsync_CreatesAllowedActionTaken_WhenDataIsValid()
+        {
+            var dto = new AllowedActionTakenSpec
+            {
+                EventTypeId = Guid.NewGuid(),
+                ActionTakenId = Guid.NewGuid(),
+                Active = true
+            };
+
+            var newAATId = await _repository.CreateAllowedActionTakenAsync(dto);
+            var results = await _repository.AllowedActionTakenExistsAsync(newAATId);
+
+
+            results.Should().BeTrue();
+        }
+
+        // DeleteAllowedActionTakenAsync
+        [Test]
+        public async Task DeleteAllowedActionTakenAsync_DeletesAllowedActionTaken_WhenIdExist()
+        {
+            var existingAAT = new AllowedActionTaken
+            {
+                Id = Guid.NewGuid(),
+                EventTypeId = Guid.NewGuid(),
+                ActionTakenId = Guid.NewGuid(),
+                Active = true
+
+            };
+            _context.AllowedActionsTaken.Add(existingAAT);
+            await _context.SaveChangesAsync();
+            _context.ChangeTracker.Clear();
+
+            await _repository.DeleteAllowedActionTakenAsync(existingAAT.Id);
+            var results = await _repository.AllowedActionTakenExistsAsync(existingAAT.Id);
+
+            results.Should().BeFalse();
+        }
+        [Test]
+        public async Task DeleteAllowedActionTakenAsync_ThrowsArgumentException_WhenIdDoesNotExist()
+        {
+            var invalidId = Guid.NewGuid();
+            var action = async () => await _repository.DeleteAllowedActionTakenAsync(invalidId);
+            await action.Should().ThrowAsync<ArgumentException>();
+        }
     }
 }
