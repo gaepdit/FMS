984a20f5a83a2be305636330f1d10bc846a0eb96
yoyocaleb <yoyocaleb1011@gmail.com>
2026-01-07 11:17:38 -0500

    SoilStatusRepositoryTest Created

---
+++ tests/FMS.Infrastructure.Tests/SoilStatusRepositoryTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NSubstitute;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FMS.Infrastructure.Contexts;
+using FMS.Infrastructure.Repositories;
+using FMS.Domain.Entities;
+using FMS.Domain.Dto;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+using System.Collections.Generic;
+
+namespace FMS.Infrastructure.Tests
+{
+    [TestFixture]
+    public class SoilStatusRepositoryTests
+    {
+        private FmsDbContext _context;
+        private SoilStatusRepository _repository;
+        private bool _disposed = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<FmsDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{Guid.NewGuid()}")
+                .Options;
+            var httpContextAccessor = Substitute.For<HttpContextAccessor>();
+            _context = new FmsDbContext(options, httpContextAccessor);
+            _repository = new SoilStatusRepository(_context);
+
+            _context.SoilStatuses.Add(new SoilStatus
+            {
+                Id = Guid.NewGuid(),
+                Name = "VALID_NAME",
+                Description = "VALID_DESCRIPTION",
+                Active = true
+            });
+            _context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _context.Database.EnsureCreated();
+                _context.Dispose();
+                _repository.Dispose();
+                _disposed = true;
+            }
+        }
+
+
+    }
+}
