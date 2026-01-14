859340148b5102bc7967a10e2322322f7252ed34
yoyocaleb <yoyocaleb1011@gmail.com>
2026-01-14 11:51:40 -0500

    Test foundation created

--- tests/FMS.Infrastructure.Tests/GroundwaterStatusRepositoryTest.cs
+++ tests/FMS.Infrastructure.Tests/GroundwaterStatusRepositoryTest.cs
@@ -1,12 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NSubstitute;
+using System;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using FMS.Infrastructure.Contexts;
+using FMS.Infrastructure.Repositories;
+using FMS.Domain.Entities;
+using FMS.Domain.Dto;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+using System.Collections.Generic;
+using FMS.Domain.Repositories;
 
 namespace FMS.Infrastructure.Tests
 {
-    internal class GroundwaterStatusRepositoryTest
+    [TestFixture]
+    public class GroundwaterStatusRepositoryTests
     {
+        private FmsDbContext _context;
+        private GroundwaterStatusRepository _repository;
+        private bool _disposed = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<FmsDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{Guid.NewGuid()}")
+                .Options;
+            var httpContextAccessor = Substitute.For<HttpContextAccessor>();
+            _context = new FmsDbContext(options, httpContextAccessor);
+            _repository = new GroundwaterStatusRepository(_context);
+
+            _context.GroundwaterStatuses.Add(new GroundwaterStatus
+            {
+                Id = Guid.NewGuid(),
+                Name = "VALID_NAME",
+                Description = "VALID_DESCRIPTION",
+                Active = true
+            });
+            _context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _context.Database.EnsureCreated();
+                _context.Dispose();
+                _repository.Dispose();
+                _disposed = true;
+            }
+        }
+
+        
     }
 }
