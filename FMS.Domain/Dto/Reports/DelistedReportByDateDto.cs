24729d550e4b76b66acdab5ec8846a8ac190d603
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-01-28 08:50:32 -0500

    Adds Events reports DTOs and repository method
    Adds several DTOs to support different event reports, including pending, completed, overdue, and compliance.
    
    Also, the changes include adding a repository method to retrieve pending events.
    
    Moves some of the DTO's to the correct folder.

--- FMS.Domain/Dto/Reports/DelistedReportByDateDto.cs
+++ FMS.Domain/Dto/Reports/DelistedReportByDateDto.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FMS.Domain.Dto.Reports
+namespace FMS.Domain.Dto
 {
     public class DelistedReportByDateDto
     {
