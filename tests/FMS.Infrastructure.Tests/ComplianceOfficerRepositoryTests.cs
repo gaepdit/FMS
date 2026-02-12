cc8bbc9547b3d9a5d8074d2b2535c8198e33c7c0
yoyocaleb <yoyocaleb1011@gmail.com>
2026-02-12 11:05:08 -0500

    ComplianceOfficerRepositoryTests.cs created

---
+++ tests/FMS.Infrastructure.Tests/ComplianceOfficerRepositoryTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using NSubstitute;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FMS.Infrastructure.Contexts;
+using FMS.Infrastructure.Repositories;
+using FMS.Domain.Entities;
+using FMS.Domain.Dto;
+using Microsoft.AspNetCore.Http;
+using FluentAssertions;
+using System.Collections.Generic;
+
+namespace FMS.Infrastructure.Tests
+{
+    [TestFixture]
+    public class ComplianceOfficerRepositoryTests
+    {
+        private FmsDbContext _context;
+        private ComplianceOfficerRepository _repository;
+        private bool _disposed = false;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<FmsDbContext>()
+                .UseInMemoryDatabase(databaseName: $"TestDatabase_{Guid.NewGuid()}")
+                .Options;
+            var httpContextAccessor = Substitute.For<HttpContextAccessor>();
+            _context = new FmsDbContext(options, httpContextAccessor);
+            _repository = new ComplianceOfficerRepository(_context);
+
+            _context.ComplianceOfficers.Add(new ComplianceOfficer
+            {
+                Id = Guid.NewGuid(),
+                GivenName = "VALID_GIVENNAME",
+                FamilyName ="VALID_FAMILYNAME",
+                Email = "VALID_EMAIL",
+                Active = true
+            });
+            _context.SaveChanges();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Dispose();
+        }
+        public void Dispose()
+        {
+            if (!_disposed)
+            {
+                _context.Database.EnsureCreated();
+                _context.Dispose();
+                _repository.Dispose();
+                _disposed = true;
+            }
+        }
+
+        
+    }
+}
