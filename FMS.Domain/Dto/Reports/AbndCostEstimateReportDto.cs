838c5ccd5a5f3d77ebe38f4edc03f6b5c3c35c5d
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-02-24 10:44:32 -0500

    Adds Abandoned/Inactive site reports
    Implements the Abandoned/Inactive Status Tracker and Abandoned Sites Cost Estimate reports.
    
    This commit introduces new DTOs for report data and a new page with buttons to generate the two reports.

---
+++ FMS.Domain/Dto/Reports/AbndCostEstimateReportDto.cs
@@ -0,0 +1,56 @@
+﻿using ClosedXML.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FMS.Domain.Dto.Reports
+{
+    public class AbndCostEstimateReportDto
+    {
+        public AbndCostEstimateReportDto() { }
+
+        [XLColumn(Header = "Class")]
+        [Display(Name = "Class")]
+        public string ClassName { get; set; }
+
+        [XLColumn(Header = "HSI ID")]
+        [Display(Name = "HSI ID")]
+        public string HSINumber { get; set; }
+        
+        [XLColumn(Header = "Facility Name")]
+        [Display(Name = "Facility Name")]
+        public string FacilityName { get; set; }
+        
+        [XLColumn(Header = "County")]
+        [Display(Name = "County")]
+        public string County { get; set; }
+
+        [XLColumn(Header = "C.O.")]
+        [Display(Name = "C.O.")]
+        public string COName { get; set; }
+
+        [XLColumn(Header = "GAPS Score")]
+        [Display(Name = "GAPS Score")]
+        public string GAPSScore { get; set; }
+
+        [XLColumn(Header = "GAPS Model Date")]
+        [Display(Name = "GAPS Model Date")]
+        public DateOnly? GAPSModelDate { get; set; }
+
+        [XLColumn(Header = "GAPS No Of Unknowns")]
+        [Display(Name = "GAPS No Of Unknowns")]
+        public string GAPSNoOfUnknowns { get; set; }
+
+        [XLColumn(Header = "GAPS Assessment")]
+        [Display(Name = "GAPS Assessment")]
+        public string GAPSAssessment { get; set; }
+
+        [XLColumn(Header = "Cost Estimate")]
+        [Display(Name = "Cost Estimate")]
+        [DataType(DataType.Currency)]
+        public decimal? CostEstimate { get; set; }
+    }
+}
