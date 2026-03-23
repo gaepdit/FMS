a48b0e9b62264bb87aadcef977d3a8e4f0460e48
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-03-23 10:57:19 -0400

    Updates GetChemicalByCasNoAsync parameter name
    Renames the parameter from 'name' to 'casno' to accurately reflect the method's purpose and the expected input.

--- FMS.Domain/Repositories/IChemicalRepository.cs
+++ FMS.Domain/Repositories/IChemicalRepository.cs
@@ -18,7 +18,7 @@ namespace FMS.Domain.Repositories
 
         Task<ChemicalEditDto> GetChemicalByIdAsync(Guid? id);
 
-        Task<Chemical> GetChemicalByCasNoAsync(string name);
+        Task<Chemical> GetChemicalByCasNoAsync(string casno);
 
         Task<Chemical> GetChemicalByChemIdAsync(Guid id);
 
