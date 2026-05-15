b9cc37b56c09b2537f10a8aeac6ff2d964b367f7
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-05-15 19:40:49 -0400

    Adds UserProgram entity and refines Site Summary display logic
    Introduces the `UserProgram` entity to enable tracking of user associations with specific organizational units or programs, a foundational step for advanced search capabilities on the dashboard.
    
    Also, adjusts the conditional logic in `SiteSummaryHelper` to ensure drinking water well information is displayed only when relevant to the groundwater score.
    
    Part of issue #954.

---
+++ FMS.Domain/Entities/Users/UserProgram.cs
@@ -0,0 +1,20 @@
+﻿using FMS.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FMS.Domain.Entities.Users
+{
+    public class UserProgram : BaseActiveModel
+    {
+        public UserProgram() { }
+
+        public UserProgram(int id) { }
+
+        public Guid UserId { get; set; }
+
+        public Guid OrgUnitId { get; set; }
+
+        public string Position { get; set; }
+    }
+}
