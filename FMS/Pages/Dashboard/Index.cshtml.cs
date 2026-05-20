262e05ac18a66181bd4ef358c0b56e49d41d104e
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-05-20 09:44:46 -0400

    Adds structured Dashboard UI and user program settings
    Replaces the placeholder Dashboard page with a more comprehensive layout including collapsible sections for current facilities, events, and data maintenance. The associated backend logic is prepared to fetch user-specific data.
    
    Also introduces an `IXmlSerializable Settings` property to the `UserProgram` entity, enabling the storage of user-specific configurations for their associated programs, which can be leveraged by the dashboard.
    
    Part of issue #954.

--- FMS/Pages/Dashboard/Index.cshtml.cs
+++ FMS/Pages/Dashboard/Index.cshtml.cs
@@ -1,12 +1,33 @@
+using FMS.Domain.Entities;
+using FMS.Domain.Repositories;
+using FMS.Domain.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FMS.Pages.Dashboard
 {
-    public class IndexModel : PageModel
+    public class IndexModel(
+        IAuthorizationService authorization, 
+        IUserService userService, 
+        IFacilityRepository _repository) : PageModel
     {
-        public void OnGet()
+        public UserView CurrentUser { get; private set; }
+
+        public string UserName {  get; set; }
+
+        public IList<Facility> UserFacilities { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
         {
+            if (User.Identity is not { IsAuthenticated: true })
+                return Challenge();
+
+            UserName = await userService.GetCurrentUserAsync() is UserView user ? user.DisplayName : "User";
+
+            //UserFacilities = await _repository.GetFacilitiesForUserAsync(user.Id);
+
+            return Page();
         }
     }
 }
