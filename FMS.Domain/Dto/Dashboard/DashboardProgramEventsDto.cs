6f3b37384521999d5b8c0e2d439a72a07535ff5e
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-06-08 14:09:07 -0400

    Add initial Dashboard DTOs and repository for facility views
    Introduces data transfer objects and a repository layer to retrieve
    facility information specific to dashboard views. This allows
    fetching facilities associated with users, organizational units,
    and programs/facility types for display. Placeholders for event-related
    DTOs are also included.
    
    Part of issue #954.

---
+++ FMS.Domain/Dto/Dashboard/DashboardProgramEventsDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FMS.Domain.Dto
+{
+    public class DashboardProgramEventsDto
+    {
+    }
+}
---
+++ FMS.Domain/Dto/Dashboard/DashboardUnitFacilitiesDto.cs
@@ -0,0 +1,20 @@
+﻿using FMS.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FMS.Domain.Dto
+{
+    public class DashboardUnitFacilitiesDto
+    {
+        public DashboardUnitFacilitiesDto(Facility facility) 
+        {
+            Id = facility.Id;
+            Name = facility.Name;
+            Active = facility.Active;
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public bool Active { get; set; }
+    }
+}
