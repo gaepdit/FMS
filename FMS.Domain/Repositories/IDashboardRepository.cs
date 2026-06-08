37673b75a667c3b68ead958cc671651f549353fa
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-06-08 14:09:07 -0400

    Add initial Dashboard DTOs and repository for facility views
    Introduces data transfer objects and a repository layer to retrieve
    facility information specific to dashboard views. This allows
    fetching facilities associated with users, organizational units,
    and programs/facility types for display. Placeholders for event-related
    DTOs are also included.
    
    Part of issue #954.

---
+++ FMS.Domain/Repositories/IDashboardRepository.cs
@@ -0,0 +1,15 @@
+﻿using FMS.Domain.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FMS.Domain.Repositories
+{
+    public interface IDashboardRepository : IDisposable
+    {
+        Task<bool> UserExistsAsync(Guid id);
+        Task<List<DashboardUserFacilitiesDto>> GetUserFacilitiesById(Guid id, bool includeInactive = false);
+        Task<List<DashboardUnitFacilitiesDto>> GetUnitFacilitiesById(Guid id, bool includeInactive = false);
+        Task<List<DashboardProgramFacilitiesDto>> GetProgramFacilitiesById(Guid id, bool includeInactive = false);
+    }
+}
---
+++ FMS.Infrastructure/Repositories/DashboardRepository.cs
@@ -0,0 +1,118 @@
+﻿using FMS.Domain.Dto;
+using FMS.Domain.Repositories;
+using FMS.Domain.Services;
+using FMS.Infrastructure.Contexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FMS.Infrastructure.Repositories
+{
+    public class DashboardRepository : IDashboardRepository
+    {
+        private readonly FmsDbContext _context;
+        public DashboardRepository(FmsDbContext context) => _context = context;
+
+        public async Task<bool> UserExistsAsync(Guid id) =>
+            await _context.Users.AnyAsync(e => e.Id == id);
+
+        public async Task<List<DashboardUserFacilitiesDto>> GetUserFacilitiesById(Guid id, bool includeInactive = false)
+        {
+            return await _context.Facilities.AsNoTracking()
+                .Include(e => e.ComplianceOfficer)
+                .Include(e => e.County)
+                .Include(e => e.Events)
+                .Include(e => e.FacilityStatus)
+                .Include(e => e.FacilityType)
+                .Include(e => e.HsrpFacilityProperties)
+                .Include(e => e.LocationDetails)
+                .ThenInclude(e => e.LocationClass)
+                .Include(e => e.OrganizationalUnit)
+                .Include(e => e.StatusDetails)
+                .ThenInclude(e => e.OverallStatus)
+                .Where(e => includeInactive || e.Active)
+                .Where(e => e.ComplianceOfficerId == id)
+                .OrderByDescending(e => e.Active)
+                .Select(e => new DashboardUserFacilitiesDto(e))
+                .ToListAsync();
+        }
+
+        public async Task<List<DashboardUnitFacilitiesDto>> GetUnitFacilitiesById(Guid id, bool includeInactive = false)
+        {
+            return await _context.Facilities.AsNoTracking()
+                .Include(e => e.ComplianceOfficer)
+                .Include(e => e.County)
+                .Include(e => e.Events)
+                .Include(e => e.FacilityStatus)
+                .Include(e => e.FacilityType)
+                .Include(e => e.HsrpFacilityProperties)
+                .Include(e => e.LocationDetails)
+                .ThenInclude(e => e.LocationClass)
+                .Include(e => e.OrganizationalUnit)
+                .Include(e => e.StatusDetails)
+                .ThenInclude(e => e.OverallStatus)
+                .Where(e => includeInactive || e.Active)
+                .Where(e => e.OrganizationalUnitId == id)
+                .OrderByDescending(e => e.Active)
+                .Select(e => new DashboardUnitFacilitiesDto(e))
+                .ToListAsync();
+        }
+
+        public async Task<List<DashboardProgramFacilitiesDto>> GetProgramFacilitiesById(Guid id, bool includeInactive = false)
+        {
+            return await _context.Facilities.AsNoTracking()
+                .Include(e => e.ComplianceOfficer)
+                .Include(e => e.County)
+                .Include(e => e.Events)
+                .Include(e => e.FacilityStatus)
+                .Include(e => e.FacilityType)
+                .Include(e => e.HsrpFacilityProperties)
+                .Include(e => e.LocationDetails)
+                .ThenInclude(e => e.LocationClass)
+                .Include(e => e.OrganizationalUnit)
+                .Include(e => e.StatusDetails)
+                .ThenInclude(e => e.OverallStatus)
+                .Where(e => includeInactive || e.Active)
+                .Where(e => e.FacilityTypeId == id)
+                .OrderByDescending(e => e.Active)
+                .Select(e => new DashboardProgramFacilitiesDto(e))
+                .ToListAsync();
+        }
+
+        #region IDisposable Support
+
+        private bool _disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposedValue) return;
+
+            if (disposing)
+            {
+                // dispose managed state (managed objects)
+                _context.Dispose();
+            }
+
+            // free unmanaged resources (unmanaged objects) and override finalizer
+            // set large fields to null
+            _disposedValue = true;
+        }
+
+        // override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        ~DashboardRepository()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: false);
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+    }
+}
