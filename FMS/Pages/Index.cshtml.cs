89d3eac61583e175e1eab98844ede281defb1f92
Doug Waldron <douglas.waldron@dnr.ga.gov>
2026-07-07 13:52:03 -0400

    Undo changes to Index
    To avoid merge conflicts with another current feature branch

--- FMS/Pages/Index.cshtml.cs
+++ FMS/Pages/Index.cshtml.cs
@@ -1,9 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FMS.Domain.Entities.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace FMS.Pages;
-
-public class IndexModel : PageModel
+namespace FMS.Pages
 {
-    public IActionResult OnGet() => LocalRedirect("~/Facilities");
+    public class IndexModel : PageModel
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public IndexModel(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult OnGet()
+        {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return LocalRedirect("~/Facilities");
+            }
+
+            return LocalRedirect("~/Account/Login");
+        }
+    }
 }
