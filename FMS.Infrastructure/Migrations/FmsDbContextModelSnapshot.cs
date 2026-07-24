13a8fb409c74c904935c9e8096b7e01b7e5a14a8
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-07-24 10:28:07 -0400

    Clean up user-related and event type migrations
    This change reverts several Entity Framework Core migration files and updates the database snapshot.
    
    Specifically, it undoes the following schema changes:
    - Removal of `UserProgram` and `UserPosition` entities.
    - Removal of `UserProgramId`, `UserPositionId`, and `UserUnitId` properties from `ApplicationUser`.
    - Removal of `UserProgramId` from `OrganizationalUnit`.
    - Removal of `Points` and `ReportEligible` properties from `EventType`.
    
    This rectifies previous migration issues.
    
    Part of issue #954.

--- FMS.Infrastructure/Migrations/FmsDbContextModelSnapshot.cs
+++ FMS.Infrastructure/Migrations/FmsDbContextModelSnapshot.cs
@@ -1330,12 +1330,6 @@ namespace FMS.Infrastructure.Migrations
                     b.Property<string>("Name")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<int>("Points")
-                        .HasColumnType("int");
-
-                    b.Property<bool>("ReportEligible")
-                        .HasColumnType("bit");
-
                     b.Property<DateTimeOffset?>("UpdateDateTime")
                         .HasColumnType("datetimeoffset");
 
@@ -1996,12 +1990,7 @@ namespace FMS.Infrastructure.Migrations
                     b.Property<string>("UpdateUser")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<Guid?>("UserProgramId")
-                        .HasColumnType("uniqueidentifier");
-
                     b.HasKey("Id");
-
-                    b.HasIndex("UserProgramId");
 
                     b.ToTable("OrganizationalUnits");
                 });
@@ -2482,70 +2471,6 @@ namespace FMS.Infrastructure.Migrations
                     b.ToTable("Substances");
                 });
 
-            modelBuilder.Entity("FMS.Domain.Entities.UserPosition", b =>
-                {
-                    b.Property<Guid>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("uniqueidentifier");
-
-                    b.Property<bool>("Active")
-                        .HasColumnType("bit");
-
-                    b.Property<string>("Description")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<DateTimeOffset?>("InsertDateTime")
-                        .HasColumnType("datetimeoffset");
-
-                    b.Property<string>("InsertUser")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<DateTimeOffset?>("UpdateDateTime")
-                        .HasColumnType("datetimeoffset");
-
-                    b.Property<string>("UpdateUser")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("UserPositions");
-                });
-
-            modelBuilder.Entity("FMS.Domain.Entities.UserProgram", b =>
-                {
-                    b.Property<Guid>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("uniqueidentifier");
-
-                    b.Property<bool>("Active")
-                        .HasColumnType("bit");
-
-                    b.Property<string>("Description")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<DateTimeOffset?>("InsertDateTime")
-                        .HasColumnType("datetimeoffset");
-
-                    b.Property<string>("InsertUser")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("Name")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<DateTimeOffset?>("UpdateDateTime")
-                        .HasColumnType("datetimeoffset");
-
-                    b.Property<string>("UpdateUser")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("UserPrograms");
-                });
-
             modelBuilder.Entity("FMS.Domain.Entities.Users.ApplicationUser", b =>
                 {
                     b.Property<Guid>("Id")
@@ -2634,15 +2559,6 @@ namespace FMS.Infrastructure.Migrations
                         .HasMaxLength(256)
                         .HasColumnType("nvarchar(256)");
 
-                    b.Property<Guid?>("UserPositionId")
-                        .HasColumnType("uniqueidentifier");
-
-                    b.Property<Guid?>("UserProgramId")
-                        .HasColumnType("uniqueidentifier");
-
-                    b.Property<Guid?>("UserUnitId")
-                        .HasColumnType("uniqueidentifier");
-
                     b.HasKey("Id");
 
                     b.HasIndex("NormalizedEmail")
@@ -2652,16 +2568,6 @@ namespace FMS.Infrastructure.Migrations
                         .IsUnique()
                         .HasDatabaseName("UserNameIndex")
                         .HasFilter("[NormalizedUserName] IS NOT NULL");
-
-                    //b.HasIndex("ObjectId")
-                    //    .IsUnique()
-                    //    .HasFilter("[ObjectId] IS NOT NULL");
-
-                    b.HasIndex("UserPositionId");
-
-                    b.HasIndex("UserProgramId");
-
-                    b.HasIndex("UserUnitId");
 
                     b.ToTable("AppUsers", (string)null);
                 });
@@ -3099,15 +3005,6 @@ namespace FMS.Infrastructure.Migrations
                     b.Navigation("Substance");
                 });
 
-            modelBuilder.Entity("FMS.Domain.Entities.OrganizationalUnit", b =>
-                {
-                    b.HasOne("FMS.Domain.Entities.UserProgram", "UserProgram")
-                        .WithMany()
-                        .HasForeignKey("UserProgramId");
-
-                    b.Navigation("UserProgram");
-                });
-
             modelBuilder.Entity("FMS.Domain.Entities.Parcel", b =>
                 {
                     b.HasOne("FMS.Domain.Entities.Facility", null)
@@ -3222,27 +3119,6 @@ namespace FMS.Infrastructure.Migrations
                         .IsRequired();
 
                     b.Navigation("Chemical");
-                });
-
-            modelBuilder.Entity("FMS.Domain.Entities.Users.ApplicationUser", b =>
-                {
-                    b.HasOne("FMS.Domain.Entities.UserPosition", "UserPosition")
-                        .WithMany()
-                        .HasForeignKey("UserPositionId");
-
-                    b.HasOne("FMS.Domain.Entities.UserProgram", "UserProgram")
-                        .WithMany()
-                        .HasForeignKey("UserProgramId");
-
-                    b.HasOne("FMS.Domain.Entities.OrganizationalUnit", "UserUnit")
-                        .WithMany()
-                        .HasForeignKey("UserUnitId");
-
-                    b.Navigation("UserPosition");
-
-                    b.Navigation("UserProgram");
-
-                    b.Navigation("UserUnit");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<System.Guid>", b =>
