6a79506629a487565d7f6cdcd21828d6d844dd8c
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-08-04 18:28:14 -0400

    Implement initial UI and data loading for `EditAATReq` page
    Sets up the new `EditAATReq` page to retrieve `AllowedActionTakenSpec` data, displaying dynamic page titles including Event Type and Action Taken names. Adds navigation back to the `EditAAT` page and establishes the basic form. Heading levels are standardized for consistent UI.
    
    This page serves as the configuration point for date requirement flags for Allowed Actions Taken, as outlined in #823.

--- FMS/Pages/Maintenance/EventType/EditAATReq.cshtml.cs
+++ FMS/Pages/Maintenance/EventType/EditAATReq.cshtml.cs
@@ -1,13 +1,40 @@
+using FMS.Domain.Dto;
+using FMS.Domain.Entities.Users;
+using FMS.Domain.Repositories;
+using FMS.Platform.Extensions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace FMS.Pages.Maintenance.EventType
 {
     public class EditAATReqModel : PageModel
     {
+        private readonly IAllowedActionTakenRepository _repository;
 
-        public void OnGet()
+        public EditAATReqModel(IAllowedActionTakenRepository repository)
         {
+            _repository = repository;
+        }
+
+        public DisplayMessage DisplayMessage { get; private set; }
+
+        public string ActionTakenName { get; private set; }
+
+        public string EventTypeName { get; private set; }
+
+        [BindProperty]
+        public AllowedActionTakenSpec AllowedActionTakenSpec { get; set; }
+
+        [FromRoute]
+        public Guid Id { get; set; }
+
+        public async Task OnGet()
+        {
+            AllowedActionTakenSpec = await _repository.GetAllowedActionTakenByAATIdAsync(Id);
+            ActionTakenName = AllowedActionTakenSpec.ActionTakenName;
+            EventTypeName = AllowedActionTakenSpec.EventTypeName;
         }
     }
 }
