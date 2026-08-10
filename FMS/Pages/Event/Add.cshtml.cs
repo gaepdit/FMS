fa46f58755e8a146e6b7225e602b63e5f60101fd
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-08-10 09:43:50 -0400

    Color completed events in Dashboard

--- FMS/Pages/Event/Add.cshtml.cs
+++ FMS/Pages/Event/Add.cshtml.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Globalization;
 
 namespace FMS.Pages.Event
 {
