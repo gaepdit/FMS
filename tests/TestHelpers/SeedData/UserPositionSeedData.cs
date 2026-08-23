c089c058536b97563bb4954a7800e1296429982c
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-08-23 16:45:28 -0400

    915: Enhance UI with sticky table headers and list group styling
    *   Implement sticky table headers (`position-sticky top-0`) across various pages to improve data visibility during scrolling.
    *   Standardize list styling on Maintenance and Reporting pages using Bootstrap's `list-group` components for improved visual consistency and readability.
    *   Reorganize the Maintenance index page links into categorized list groups for better navigation.
    *   Perform minor UI cleanups by removing redundant `` elements.
    *   Correct 'Facility Type/Environmental Interest' display text.

---
+++ tests/TestHelpers/SeedData/UserPositionSeedData.cs
@@ -0,0 +1,22 @@
+﻿using FMS.Domain.Entities;
+
+// ReSharper disable StringLiteralTypo
+
+namespace FMS.TestData.SeedData
+{
+    public static partial class SeedData
+    {
+        public static List<UserPosition> GetUserPositions()
+        {
+            return new List<UserPosition>()
+            {
+                new()
+                {
+                    Id = new Guid(""),
+                    Active = true,
+                    
+                },
+            };
+        }
+    }
+}
---
+++ tests/TestHelpers/SeedData/UserProgramSeedData.cs
@@ -0,0 +1,22 @@
+﻿using FMS.Domain.Entities;
+
+// ReSharper disable StringLiteralTypo
+
+namespace FMS.TestData.SeedData
+{
+    public static partial class SeedData
+    {
+        public static List<UserProgram> GetUserPrograms()
+        {
+            return new List<UserProgram>()
+            {
+                new()
+                {
+                    Id = new Guid(""),
+                    Active = true,
+
+                },
+            };
+        }
+    }
+}
