a4aa771c2d2d198dc38b752cda04510830516f54
Tom Karasch <tom.karasch@dnr.ga.gov>
2026-08-27 17:30:33 -0400

    1115: Introduce delete buttons for contacts and phone numbers
    Adds 'Delete' buttons to the Facility Details page for contacts and phone numbers. These buttons are only visible to Program Managers (PM1 or PM2) who also possess File Editor or Compliance Officer roles, and only when the facility is active. Basic Razor Pages for contact and phone deletion have been scaffolded.

---
+++ FMS/Pages/Contact/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMS.Pages.Contact
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ FMS/Pages/Phone/Delete.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FMS.Pages.Phone
+{
+    public class DeleteModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
